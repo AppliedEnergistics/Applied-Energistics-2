22fafa697abb9ac579a9fc8a5bbcbed96c7c1ec5
AlgorithmX2 <AlgorithmX2@gmail.com>
2013-12-28 14:54:53 -0600

    Better args

--- container/slot/SlotNormal.java
+++ container/slot/SlotNormal.java
@@ -1,14 +1,12 @@
 package appeng.container.slot;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 
 public class SlotNormal extends AppEngSlot
 {
 
-	public SlotNormal(Container _c, EntityPlayer _p, IInventory par1iInventory, int par2, int par3, int par4) {
-		super( par1iInventory, par2, par3, par4 );
+	public SlotNormal(IInventory inv, int slot, int xPos, int yPos) {
+		super( inv, slot, xPos, yPos );
 	}
 
 }
