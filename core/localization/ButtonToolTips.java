71f95b7d4ed3fb638ac0cde6a359720a3789ef40
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-30 13:03:40 -0600

    Forgot a file.

---
+++ core/localization/ButtonToolTips.java
@@ -0,0 +1,57 @@
+package appeng.core.localization;
+
+import net.minecraft.util.StatCollector;
+import appeng.core.AELog;
+
+public enum ButtonToolTips
+{
+	PowerUnits, IOMode, CondenserOutput, RedstoneMode, MatchingFuzzy,
+
+	MatchingMode, TransferDirection, SortOrder, SortBy, View,
+
+	PartitionStorage, Clear, FuzzyMode, OperationMode, TrashController,
+
+	InterfaceBlockingMode, InterfaceCraftingMode, Trash, MatterBalls,
+
+	Singularity, Read, Write, ReadWrite, AlwaysActive,
+
+	ActiveWithoutSignal, ActiveWithSignal, ActiveOnPulse,
+
+	EmitLevelsBelow, EmitLevelAbove, MatchingExact, TransferToNetwork,
+
+	TransferToStorageCell, ToggleSortDirection, SearchMode_Auto,
+
+	SearchMode_Standard, SearchMode_NEIAuto, SearchMode_NEIStandard,
+
+	SearchMode, ItemName, NumberOfItems, PartitionStorageHint,
+
+	ClearSettings, StoredItems, StoredCraftable, Craftable,
+
+	FZPercent_25, FZPercent_50, FZPercent_75, FZPercent_99, FZIgnoreAll,
+
+	MoveWhenEmpty, MoveWhenWorkIsDone, MoveWhenFull, Disabled, Enable,
+
+	Blocking, NonBlocking, Craft, DontCraft;
+
+	String root;
+
+	ButtonToolTips() {
+		root = "gui.tooltips.appliedenergistics2";
+		AELog.localization( "gui.tooltips", getUnlocalized() );
+	}
+
+	ButtonToolTips(String r) {
+		root = r;
+	}
+
+	public String getUnlocalized()
+	{
+		return root + "." + toString();
+	}
+
+	public String getLocal()
+	{
+		return StatCollector.translateToLocal( getUnlocalized() );
+	}
+
+}
