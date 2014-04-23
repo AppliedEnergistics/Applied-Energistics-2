8a5a4b30972fe24e3384c91cadf44312dfb40210
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-04-22 20:36:07 -0500

    Prioritize Pattern Slots.

--- container/implementations/ContainerInterface.java
+++ container/implementations/ContainerInterface.java
@@ -19,14 +19,14 @@ public class ContainerInterface extends AEBaseContainer
 		super( ip, te.getInterfaceDuality().getTile(), te.getInterfaceDuality().getPart() );
 		myte = te.getInterfaceDuality();
 
+		for (int x = 0; x < 9; x++)
+			addSlotToContainer( new SlotRestrictedInput( PlaceableItemType.ENCODED_PATTERN, myte.getPatterns(), x, 8 + 18 * x, 90 + 7 ) );
+
 		for (int x = 0; x < 8; x++)
 			addSlotToContainer( new SlotFake( myte.getConfig(), x, 17 + 18 * x, 35 ) );
 
 		for (int x = 0; x < 8; x++)
 			addSlotToContainer( new SlotNormal( myte.getStorage(), x, 17 + 18 * x, 35 + 18 ) );
-
-		for (int x = 0; x < 9; x++)
-			addSlotToContainer( new SlotRestrictedInput( PlaceableItemType.ENCODED_PATTERN, myte.getPatterns(), x, 8 + 18 * x, 90 + 7 ) );
 
 		bindPlayerInventory( ip, 0, 211 - /* height of playerinventory */82 );
 	}
