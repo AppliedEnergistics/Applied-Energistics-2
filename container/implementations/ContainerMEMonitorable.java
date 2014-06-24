93b0c9bac01a9056381eec528f43771d50903ad5
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-06-24 12:00:12 -0500

    Marked Terminal as InvTweaks Proof hopefully no more duping items?

--- container/implementations/ContainerMEMonitorable.java
+++ container/implementations/ContainerMEMonitorable.java
@@ -1,5 +1,7 @@
 package appeng.container.implementations;
 
+import invtweaks.api.container.IgnoreContainer;
+
 import java.io.IOException;
 import java.nio.BufferOverflowException;
 
@@ -45,6 +47,7 @@ import appeng.util.ConfigManager;
 import appeng.util.IConfigManagerHost;
 import appeng.util.Platform;
 
+@IgnoreContainer
 public class ContainerMEMonitorable extends AEBaseContainer implements IConfigManagerHost, IConfigureableObject, IMEMonitorHandlerReceiver<IAEItemStack>
 {
 
