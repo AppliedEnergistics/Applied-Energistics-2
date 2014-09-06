9df9ffdc25b24aa9a3a89a0663a1b1182145a2d1
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-09-06 17:00:41 -0500

    Inverted Toggle buses no longer drain power.

--- parts/misc/PartInvertedToggleBus.java
+++ parts/misc/PartInvertedToggleBus.java
@@ -7,6 +7,8 @@ public class PartInvertedToggleBus extends PartToggleBus
 
 	public PartInvertedToggleBus(ItemStack is) {
 		super( PartInvertedToggleBus.class, is );
+		proxy.setIdlePowerUsage( 0.0 );
+		outerProxy.setIdlePowerUsage( 0.0 );
 		proxy.setFlags();
 		outerProxy.setFlags();
 	}
