07d106cb03f31527f7389e1353be083f0dccf11c
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-09-10 14:42:13 -0500

    Cable Anchor Ladders no longer stop being ladders when you stop going up.

--- parts/misc/PartCableAnchor.java
+++ parts/misc/PartCableAnchor.java
@@ -218,7 +218,7 @@ public class PartCableAnchor implements IPart
 	@Override
 	public boolean isLadder(EntityLivingBase entity)
 	{
-		return mySide.offsetY == 0 && entity.isCollidedHorizontally;
+		return mySide.offsetY == 0 && ( entity.isCollidedHorizontally || ! entity.onGround );
 	}
 
 	@Override
