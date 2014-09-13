37aaa8776f3a7e5928a48e1faa5cfae8434d1d6d
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-09-13 15:18:56 -0500

    Update CubeGenerator to use new tile system.

--- debug/TileCubeGenerator.java
+++ debug/TileCubeGenerator.java
@@ -1,6 +1,5 @@
 package appeng.debug;
 
-
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,7 +7,7 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.util.ForgeDirection;
 import appeng.core.CommonHelper;
 import appeng.tile.AEBaseTile;
-import appeng.tile.events.AETileEventHandler;
+import appeng.tile.TileEvent;
 import appeng.tile.events.TileEventType;
 import appeng.util.Platform;
 
@@ -19,82 +18,70 @@ public class TileCubeGenerator extends AEBaseTile
 	ItemStack is = null;
 	int countdown = 20 * 10;
 	EntityPlayer who;
-	
-	class TCG extends AETileEventHandler {
-		
-		public TCG() {
-			super(TileEventType.TICK);
-		}
-		
-		@Override
-		public void Tick()
+
+	@TileEvent(TileEventType.TICK)
+	public void TCG_Tick()
+	{
+		if ( is != null && Platform.isServer() )
 		{
-			if ( is != null && Platform.isServer() )
+			countdown--;
+
+			if ( countdown % 20 == 0 )
 			{
-				countdown--;
-				
-				if (countdown % 20 == 0 )
+				for (EntityPlayer e : CommonHelper.proxy.getPlayers())
 				{
-					for ( EntityPlayer e : CommonHelper.proxy.getPlayers() )
-					{
-						e.addChatMessage(new ChatComponentText("Spawning in... "+( countdown / 20 )));
-					}
+					e.addChatMessage( new ChatComponentText( "Spawning in... " + (countdown / 20) ) );
 				}
-				
-				if ( countdown <= 0 )
-					spawn();
 			}
+
+			if ( countdown <= 0 )
+				spawn();
 		}
-		
-	};
-	
+	}
+
 	void spawn()
 	{
 		worldObj.setBlock( xCoord, yCoord, zCoord, Platform.air, 0, 3 );
-		
+
 		Item i = is.getItem();
 		int side = ForgeDirection.UP.ordinal();
-		
+
 		int half = (int) Math.floor( size / 2 );
-		
-		for ( int y = 0; y < size; y++ )
+
+		for (int y = 0; y < size; y++)
 		{
-			for ( int x = -half; x < half; x++ )
+			for (int x = -half; x < half; x++)
 			{
-				for ( int z = -half; z < half; z++ )
+				for (int z = -half; z < half; z++)
 				{
-					i.onItemUse( is.copy(), who, worldObj, x+xCoord, y+yCoord-1, z+zCoord, side, 0.5f, 0.0f, 0.5f );
+					i.onItemUse( is.copy(), who, worldObj, x + xCoord, y + yCoord - 1, z + zCoord, side, 0.5f, 0.0f, 0.5f );
 				}
 			}
 		}
 	}
-	
-	public TileCubeGenerator() {
-		addNewHandler( new TCG() );
-	}
-	
+
 	public void click(EntityPlayer player)
 	{
-		if (  Platform.isServer() )
+		if ( Platform.isServer() )
 		{
 			ItemStack hand = player.inventory.getCurrentItem();
 			who = player;
-			
+
 			if ( hand == null )
 			{
 				is = null;
-				
+
 				if ( player.isSneaking() )
-					size --;
+					size--;
 				else
 					size++;
-				
+
 				if ( size < 3 )
 					size = 3;
 				if ( size > 64 )
 					size = 64;
-				
-				player.addChatMessage( new ChatComponentText("Size: "+size) );
+
+				player.addChatMessage( new ChatComponentText( "Size: " + size ) );
 			}
 			else
 			{
