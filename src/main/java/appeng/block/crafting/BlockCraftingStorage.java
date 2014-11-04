53b774469585beaccfb00f2318dd365a1a4023e6
thatsIch <badhard@arcor.de>
2014-11-04 02:33:31 +0100

    Better specification which class is returned

--- src/main/java/appeng/block/crafting/BlockCraftingStorage.java
+++ src/main/java/appeng/block/crafting/BlockCraftingStorage.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Applied Energistics 2.
+ * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ *
+ * Applied Energistics 2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Applied Energistics 2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Applied Energistics 2.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
 package appeng.block.crafting;
 
 import java.util.List;
@@ -13,14 +31,15 @@ import appeng.tile.crafting.TileCraftingStorageTile;
 
 public class BlockCraftingStorage extends BlockCraftingUnit
 {
-
-	public BlockCraftingStorage() {
+	public BlockCraftingStorage()
+	{
 		super( BlockCraftingStorage.class );
+
 		setTileEntity( TileCraftingStorageTile.class );
 	}
 
 	@Override
-	public Class getItemBlockClass()
+	public Class<ItemCraftingStorage> getItemBlockClass()
 	{
 		return ItemCraftingStorage.class;
 	}
@@ -56,7 +75,7 @@ public class BlockCraftingStorage extends BlockCraftingUnit
 		case 3:
 			return ExtraBlockTextures.BlockCraftingStorage64k.getIcon();
 
-		case 0 | FLAG_FORMED:
+		case FLAG_FORMED:
 			return ExtraBlockTextures.BlockCraftingStorage1kFit.getIcon();
 		case 1 | FLAG_FORMED:
 			return ExtraBlockTextures.BlockCraftingStorage4kFit.getIcon();
