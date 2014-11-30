8717411f99a20d1378f2f513a6a7cee4d03cfadf
thatsIch <badhard@arcor.de>
2014-11-30 15:21:52 +0100

    Always 0

--- src/main/java/appeng/client/gui/widgets/GuiTabButton.java
+++ src/main/java/appeng/client/gui/widgets/GuiTabButton.java
@@ -83,16 +83,15 @@ public class GuiTabButton extends GuiButton implements ITooltip
 			minecraft.renderEngine.bindTexture( ExtraBlockTextures.GuiTexture( "guis/states.png" ) );
 			this.field_146123_n = x >= this.xPosition && y >= this.yPosition && x < this.xPosition + this.width && y < this.yPosition + this.height;
 
-			int uv_y = ( int ) Math.floor( 13 / 16 );
-			int uv_x = ( this.hideEdge > 0 ? 11 : 13 ) - uv_y * 16;
+			int uv_x = ( this.hideEdge > 0 ? 11 : 13 );
 
 			int offsetX = this.hideEdge > 0 ? 1 : 0;
 
-			this.drawTexturedModalRect( this.xPosition, this.yPosition, uv_x * 16, uv_y * 16, 25, 22 );
+			this.drawTexturedModalRect( this.xPosition, this.yPosition, uv_x * 16, 0, 25, 22 );
 
 			if ( this.myIcon >= 0 )
 			{
-				uv_y = ( int ) Math.floor( this.myIcon / 16 );
+				int uv_y = ( int ) Math.floor( this.myIcon / 16 );
 				uv_x = this.myIcon - uv_y * 16;
 
 				this.drawTexturedModalRect( offsetX + this.xPosition + 3, this.yPosition + 3, uv_x * 16, uv_y * 16, 16, 16 );
