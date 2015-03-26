db7296c0a13ea16d3888f6497bb7b0f9e2c30749
thatsIch <badhard@arcor.de>
2015-03-26 13:57:31 +0100

    Final classes with private constructor

--- src/main/java/appeng/entity/EntityIds.java
+++ src/main/java/appeng/entity/EntityIds.java
@@ -18,25 +18,30 @@
 
 package appeng.entity;
 
+
 import net.minecraft.entity.Entity;
 
-public class EntityIds
-{
 
+public final class EntityIds
+{
 	public static final int TINY_TNT = 10;
 	public static final int SINGULARITY = 11;
 	public static final int CHARGED_QUARTZ = 12;
 	public static final int GROWING_CRYSTAL = 13;
 
-	public static int get(Class<? extends Entity> droppedEntity)
+	private EntityIds()
 	{
-		if ( droppedEntity == EntityTinyTNTPrimed.class )
+	}
+
+	public static int get( Class<? extends Entity> droppedEntity )
+	{
+		if( droppedEntity == EntityTinyTNTPrimed.class )
 			return TINY_TNT;
-		if ( droppedEntity == EntitySingularity.class )
+		if( droppedEntity == EntitySingularity.class )
 			return SINGULARITY;
-		if ( droppedEntity == EntityChargedQuartz.class )
+		if( droppedEntity == EntityChargedQuartz.class )
 			return CHARGED_QUARTZ;
-		if ( droppedEntity == EntityGrowingCrystal.class )
+		if( droppedEntity == EntityGrowingCrystal.class )
 			return GROWING_CRYSTAL;
 
 		throw new RuntimeException( "Missing entity id: " + droppedEntity.getName() );
