3327fd5a831b7c4b92ce0746ca14ffe326fe8fb2
PetaByteBoy <petabyteboy@gmail.com>
2015-05-10 20:35:58 +0200

    remove debug prints

--- src/main/java/appeng/block/AEBaseItemBlockSlab.java
+++ src/main/java/appeng/block/AEBaseItemBlockSlab.java
@@ -7,9 +7,5 @@ public class AEBaseItemBlockSlab extends ItemSlab {
 
 	public AEBaseItemBlockSlab(Block b, AEBaseSlabBlock sb, AEBaseSlabBlock db, Boolean id) {
 		super( b, sb, db, id );
-		System.out.println(b);
-		System.out.println(sb);
-		System.out.println(db);
-		System.out.println(id);
 	}
 }
