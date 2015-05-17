3613c235be7f13ccd8bfe6bef414f2d734c13051
thatsIch <badhard@arcor.de>
2015-05-18 00:34:37 +0200

    Unused imports

--- src/main/java/appeng/core/features/registries/entries/BasicCellHandler.java
+++ src/main/java/appeng/core/features/registries/entries/BasicCellHandler.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Applied Energistics 2.
- * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ * Copyright (c) 2013 - 2015, AlgorithmX2, All rights reserved.
  *
  * Applied Energistics 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -36,7 +36,6 @@ import appeng.client.texture.ExtraBlockTextures;
 import appeng.core.sync.GuiBridge;
 import appeng.me.storage.CellInventory;
 import appeng.me.storage.CellInventoryHandler;
-import appeng.tile.AEBaseTile;
 import appeng.util.Platform;
 
 
--- src/main/java/appeng/tile/networking/TileCableBus.java
+++ src/main/java/appeng/tile/networking/TileCableBus.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Applied Energistics 2.
- * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ * Copyright (c) 2013 - 2015, AlgorithmX2, All rights reserved.
  *
  * Applied Energistics 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,6 @@ package appeng.tile.networking;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
