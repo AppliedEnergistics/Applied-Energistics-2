733a53ec6d77ee8f8fc584cc2b93627dfd07bee0
thatsIch <badhard@arcor.de>
2015-05-18 00:33:04 +0200

    Typo in 'fward'

--- src/main/java/appeng/helpers/MetaRotation.java
+++ src/main/java/appeng/helpers/MetaRotation.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Applied Energistics 2.
- * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ * Copyright (c) 2013 - 2015, AlgorithmX2, All rights reserved.
  *
  * Applied Energistics 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -65,7 +65,7 @@ public class MetaRotation implements IOrientable
 	}
 
 	@Override
-	public void setOrientation( ForgeDirection foward, ForgeDirection up )
+	public void setOrientation( ForgeDirection forward, ForgeDirection up )
 	{
 		if( this.w instanceof World )
 		{
