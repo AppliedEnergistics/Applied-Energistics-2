abdd6e191552b99f203caf19c0d2732ced4cd8b2
thatsIch <badhard@arcor.de>
2015-05-18 00:31:41 +0200

    Typo in 'constructor'

--- src/main/java/appeng/core/features/BlockDefinition.java
+++ src/main/java/appeng/core/features/BlockDefinition.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Applied Energistics 2.
- * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ * Copyright (c) 2013 - 2015, AlgorithmX2, All rights reserved.
  *
  * Applied Energistics 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -102,7 +102,7 @@ public class BlockDefinition extends ItemDefinition implements IBlockDefinition
 	}
 
 	/**
-	 * Returns the cosntructor to use.
+	 * Returns the constructor to use.
 	 *
 	 * Either {@link ItemBlock} or in case of an {@link AEBaseBlock} the class returned by
 	 * AEBaseBlock.getItemBlockClass().
