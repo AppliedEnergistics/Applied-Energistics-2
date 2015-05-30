69aff21f6aa0705ecfb6df2c4d51fb57df6da93f
thatsIch <badhard@arcor.de>
2015-05-30 22:28:13 +0200

    Might fix #397: Now file name of meteor data is stored correctly

--- src/main/java/appeng/core/WorldSettings.java
+++ src/main/java/appeng/core/WorldSettings.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Applied Energistics 2.
- * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ * Copyright (c) 2013 - 2015, AlgorithmX2, All rights reserved.
  *
  * Applied Energistics 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -165,7 +165,7 @@ public class WorldSettings extends Configuration
 		}
 
 		NBTTagCompound data = null;
-		File file = new File( this.spawnDataFolder, dim + '_' + ( chunkX >> 4 ) + '_' + ( chunkZ >> 4 ) + ".dat" );
+		File file = new File( this.spawnDataFolder, dim + "_" + ( chunkX >> 4 ) + '_' + ( chunkZ >> 4 ) + ".dat" );
 
 		if( file.isFile() )
 		{
@@ -233,7 +233,7 @@ public class WorldSettings extends Configuration
 			throw new IllegalStateException( "Invalid Request" );
 		}
 
-		File file = new File( this.spawnDataFolder, dim + '_' + ( chunkX >> 4 ) + '_' + ( chunkZ >> 4 ) + ".dat" );
+		File file = new File( this.spawnDataFolder, dim + "_" + ( chunkX >> 4 ) + '_' + ( chunkZ >> 4 ) + ".dat" );
 		FileOutputStream fileOutputStream = null;
 
 		try
