96a9e2f55838b767e20f48396373ae3bfbd26847
Sebastian Hartte <sebastian@hartte.de>
2016-08-26 14:02:24 +0200

    Fixed side-scoping issue that prevented the server to start up.

--- src/main/java/appeng/block/misc/SkyCompassRendering.java
+++ src/main/java/appeng/block/misc/SkyCompassRendering.java
@@ -2,6 +2,8 @@ package appeng.block.misc;
 
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import appeng.bootstrap.BlockRenderingCustomizer;
 import appeng.bootstrap.IBlockRendering;
@@ -16,6 +18,7 @@ public class SkyCompassRendering extends BlockRenderingCustomizer
 	private static final ModelResourceLocation ITEM_MODEL = new ModelResourceLocation( "appliedenergistics2:sky_compass", "normal" );
 
 	@Override
+	@SideOnly( Side.CLIENT )
 	public void customize( IBlockRendering rendering, IItemRendering itemRendering )
 	{
 		rendering.tesr( new SkyCompassTESR() );
@@ -24,4 +27,5 @@ public class SkyCompassRendering extends BlockRenderingCustomizer
 		itemRendering.model( ITEM_MODEL );
 		itemRendering.builtInModel( "models/block/builtin/sky_compass", new SkyCompassModel() );
 	}
+
 }
