4f53f5910b38e887a5a89628a050e99c295bedc7
Sebastian Hartte <sebastian@hartte.de>
2016-10-30 15:29:51 +0100

    Fixes #2536: Mark host for save when placing facades.

--- src/main/java/appeng/parts/PartPlacement.java
+++ src/main/java/appeng/parts/PartPlacement.java
@@ -172,6 +172,7 @@ public class PartPlacement
 						{
 							if( host.getFacadeContainer().addFacade( fp ) )
 							{
+								host.markForSave();
 								host.markForUpdate();
 								if( !player.capabilities.isCreativeMode )
 								{
