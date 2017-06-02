ccc46e77a39be8ec515552cda86f218ed32f2e03
Gunther De Wachter <GuntherDW@gmail.com>
2017-06-03 00:01:01 +0200

    Fix the coremod looking for the wrong minecraft version

--- src/main/java/appeng/coremod/AppEngCore.java
+++ src/main/java/appeng/coremod/AppEngCore.java
@@ -37,7 +37,7 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import appeng.core.AEConfig;
 
 
-@MCVersion( "1.11" )
+@MCVersion( "1.11.2" )
 public final class AppEngCore extends DummyModContainer implements IFMLLoadingPlugin
 {
 	private final ModMetadata metadata = new ModMetadata();
