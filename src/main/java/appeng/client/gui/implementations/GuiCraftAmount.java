c74aaddbd3a4268a83ac8965ca3a8bd061437a26
Gunther De Wachter <GuntherDW@gmail.com>
2017-06-24 16:50:26 +0200

    Fixed texture issue for the crafting window.

--- src/main/java/appeng/client/gui/implementations/GuiCraftAmount.java
+++ src/main/java/appeng/client/gui/implementations/GuiCraftAmount.java
@@ -148,7 +148,7 @@ public class GuiCraftAmount extends AEBaseGui
 	{
 		this.next.displayString = isShiftKeyDown() ? GuiText.Start.getLocal() : GuiText.Next.getLocal();
 
-		this.bindTexture( "guis/craftAmt.png" );
+		this.bindTexture( "guis/craft_amt.png" );
 		this.drawTexturedModalRect( offsetX, offsetY, 0, 0, this.xSize, this.ySize );
 
 		try
