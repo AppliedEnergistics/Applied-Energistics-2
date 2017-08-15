04d2b06c0d5ffb5e4f6ae0adc87a59d5b6005342
fscan <flo@chilicode.com>
2017-08-15 15:07:42 +0200

    Ignore MOVE_REGION when clicking on crafting slots (#3032)
    Exclude crafting slots from list of slots to move on space click.
    
    Fixes #3031

--- src/main/java/appeng/container/AEBaseContainer.java
+++ src/main/java/appeng/container/AEBaseContainer.java
@@ -828,11 +828,11 @@ public abstract class AEBaseContainer extends Container
 
 			if( action == InventoryAction.MOVE_REGION )
 			{
-				final List<Slot> from = new LinkedList<>();
+				final List<Slot> from = new ArrayList<>();
 
 				for( final Object j : this.inventorySlots )
 				{
-					if( j instanceof Slot && j.getClass() == s.getClass() )
+					if( j instanceof Slot && j.getClass() == s.getClass() && !( j instanceof SlotCraftingTerm ) )
 					{
 						from.add( (Slot) j );
 					}
