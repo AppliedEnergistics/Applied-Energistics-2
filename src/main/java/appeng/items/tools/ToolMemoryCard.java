7cb641cdc822839bb70fb6b99464a3d6faf8b39c
Gunther De Wachter <GuntherDW@gmail.com>
2017-08-16 03:23:18 +0200

    Fix shift clicking memory card still triggering off-hand item/block usage.

--- src/main/java/appeng/items/tools/ToolMemoryCard.java
+++ src/main/java/appeng/items/tools/ToolMemoryCard.java
@@ -146,11 +146,14 @@ public class ToolMemoryCard extends AEBaseItem implements IMemoryCard
 	@Override
 	public EnumActionResult onItemUse( final EntityPlayer player, final World w, final BlockPos pos, final EnumHand hand, final EnumFacing side, final float hx, final float hy, final float hz )
 	{
-		if( player.isSneaking() && !w.isRemote )
+		if( player.isSneaking() )
 		{
-			final IMemoryCard mem = (IMemoryCard) player.getHeldItem( hand ).getItem();
-			mem.notifyUser( player, MemoryCardMessages.SETTINGS_CLEARED );
-			player.getHeldItem( hand ).setTagCompound( null );
+			if( !w.isRemote )
+			{
+				final IMemoryCard mem = (IMemoryCard) player.getHeldItem( hand ).getItem();
+				mem.notifyUser( player, MemoryCardMessages.SETTINGS_CLEARED );
+				player.getHeldItem( hand ).setTagCompound( null );
+			}
 			return EnumActionResult.SUCCESS;
 		}
 		else
