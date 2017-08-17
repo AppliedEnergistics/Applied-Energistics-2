80eaa032b99c191d3c102872917bd9493483e255
Gunther De Wachter <GuntherDW@gmail.com>
2017-08-17 22:19:35 +0200

    Fixes #3011: placing the last part in off-hand clearing main hand as well

--- src/main/java/appeng/parts/PartPlacement.java
+++ src/main/java/appeng/parts/PartPlacement.java
@@ -349,10 +349,9 @@ public class PartPlacement
 				if( !player.capabilities.isCreativeMode )
 				{
 					held.grow( -1 );
-					;
 					if( held.getCount() == 0 )
 					{
-						player.inventory.mainInventory.set( player.inventory.currentItem, ItemStack.EMPTY );
+						player.setHeldItem( hand, ItemStack.EMPTY );
 						MinecraftForge.EVENT_BUS.post( new PlayerDestroyItemEvent( player, held, hand ) );
 					}
 				}
