8b9bbf100f83c6db2eb0cae5e88e6f3b33c8ce86
yueh <yueh@users.noreply.github.com>
2018-01-09 20:21:43 +0100

    Cleanup

--- src/main/java/appeng/util/helpers/ItemComparisonHelper.java
+++ src/main/java/appeng/util/helpers/ItemComparisonHelper.java
@@ -25,8 +25,6 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTUtil;
 import net.minecraftforge.oredict.OreDictionary;
 
 import appeng.api.config.FuzzyMode;
