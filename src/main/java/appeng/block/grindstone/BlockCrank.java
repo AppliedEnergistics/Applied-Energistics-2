eb3dcfe02ca0501e1b20a672332811fcd9feeadc
yueh <yueh@users.noreply.github.com>
2018-01-28 13:38:10 +0100

    Fixes #3239: Torches should not be placeable on cranks.

--- src/main/java/appeng/block/grindstone/BlockCrank.java
+++ src/main/java/appeng/block/grindstone/BlockCrank.java
@@ -23,6 +23,7 @@ import javax.annotation.Nullable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -32,6 +33,7 @@ import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 
@@ -162,6 +164,18 @@ public class BlockCrank extends AEBaseTileBlock
 	public boolean isFullCube( IBlockState state )
 	{
 		return false;
+	}
+
+	@Override
+	public boolean canPlaceTorchOnTop( IBlockState state, IBlockAccess world, BlockPos pos )
+	{
+		return false;
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape( IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face )
+	{
+		return BlockFaceShape.UNDEFINED;
 	}
 
 }
