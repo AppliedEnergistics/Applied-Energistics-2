coding-1ab/modpack-season-2

Fixes #2547: Crystal growing and forming fluix crystals was not possible on top of an annihilation plane (or any block with a not-quite-full bounding box), because the crystals thought they were not in water. Fixed by using the center of the crystal bounding box to determine water-status, instead of the bottom center.

mods/Applied-Energistics-2/src/main/java/appeng/entity/EntityChargedQuartz.java
@@ -77,7 +77,7 @@ public final class EntityChargedQuartz extends AEBaseEntityItem
 		this.delay++;
 
 		final int j = MathHelper.floor_double( this.posX );
-		final int i = MathHelper.floor_double( this.posY );
+		final int i = MathHelper.floor_double( (this.getEntityBoundingBox().minY + this.getEntityBoundingBox().maxY) / 2.0D );
 		final int k = MathHelper.floor_double( this.posZ );
 
 		IBlockState state = this.worldObj.getBlockState( new BlockPos( j, i, k ) );

mods/Applied-Energistics-2/src/main/java/appeng/entity/EntityGrowingCrystal.java
@@ -70,7 +70,7 @@ public final class EntityGrowingCrystal extends EntityItem
 		if( gc instanceof IGrowableCrystal ) // if it changes this just stops being an issue...
 		{
 			final int j = MathHelper.floor_double( this.posX );
-			final int i = MathHelper.floor_double( this.posY );
+			final int i = MathHelper.floor_double( (this.getEntityBoundingBox().minY + this.getEntityBoundingBox().maxY) / 2.0D );
 			final int k = MathHelper.floor_double( this.posZ );
 
 			final IBlockState state = this.worldObj.getBlockState( new BlockPos( j, i, k ) );

AppliedEnergistics/Applied-Energistics-2

(cherry picked from commit 2b02dc19c07c10b1a790465115be738d10923746)

src/main/java/appeng/entity/EntityChargedQuartz.java
@@ -74,7 +74,7 @@ public final class EntityChargedQuartz extends AEBaseEntityItem
 		this.delay++;
 
 		final int j = MathHelper.floor_double( this.posX );
-		final int i = MathHelper.floor_double( this.posY );
+		final int i = MathHelper.floor_double( (this.getBoundingBox().minY + this.getBoundingBox().maxY) / 2.0D );
 		final int k = MathHelper.floor_double( this.posZ );
 
 		final Material mat = this.worldObj.getBlock( j, i, k ).getMaterial();

src/main/java/appeng/entity/EntityGrowingCrystal.java
@@ -72,7 +72,7 @@ public final class EntityGrowingCrystal extends EntityItem
 		if( gc instanceof IGrowableCrystal ) // if it changes this just stops being an issue...
 		{
 			final int j = MathHelper.floor_double( this.posX );
-			final int i = MathHelper.floor_double( this.posY );
+			final int i = MathHelper.floor_double( (this.getBoundingBox().minY + this.getBoundingBox().maxY) / 2.0D );
 			final int k = MathHelper.floor_double( this.posZ );
 
 			final Block blk = this.worldObj.getBlock( j, i, k );