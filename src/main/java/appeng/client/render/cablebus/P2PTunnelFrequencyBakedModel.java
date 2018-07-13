24a253a3859b62229926aac38163b513fda18629
Florian Scandella <flo@chilicode.com>
2018-07-13 02:02:59 +0200

    render full color code in each corner, add a small cache

--- src/main/java/appeng/client/render/cablebus/P2PTunnelFrequencyBakedModel.java
+++ src/main/java/appeng/client/render/cablebus/P2PTunnelFrequencyBakedModel.java
@@ -2,9 +2,12 @@
 package appeng.client.render.cablebus;
 
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -24,6 +27,8 @@ public class P2PTunnelFrequencyBakedModel implements IBakedModel, IPartBakedMode
 	private final VertexFormat format;
 	private final TextureAtlasSprite texture;
 
+	private final static Cache<Long, List<BakedQuad>> modelCache = CacheBuilder.newBuilder().maximumSize( 100 ).build();
+
 	private static final int[][] QUAD_OFFSETS = new int[][] {
 			{ 4, 10, 2 },
 			{ 10, 10, 2 },
@@ -40,14 +45,24 @@ public class P2PTunnelFrequencyBakedModel implements IBakedModel, IPartBakedMode
 	@Override
 	public List<BakedQuad> getPartQuads( Long partFlags, long rand )
 	{
-		short frequency = 0;
-		boolean active = false;
-		if( partFlags != null )
+		try
 		{
-			frequency = (short) ( partFlags.longValue() & 0xffffL );
-			active = ( partFlags.longValue() & 0x10000L ) != 0;
+			return modelCache.get( partFlags, () ->
+			{
+				short frequency = 0;
+				boolean active = false;
+				if( partFlags != null )
+				{
+					frequency = (short) ( partFlags.longValue() & 0xffffL );
+					active = ( partFlags.longValue() & 0x10000L ) != 0;
+				}
+				return getQuadsForFrequency( frequency, active );
+			} );
 		}
-		return getQuadsForFrequency( frequency, active );
+		catch( ExecutionException e )
+		{
+			return Collections.emptyList();
+		}
 	}
 
 	@Override
@@ -62,29 +77,37 @@ public class P2PTunnelFrequencyBakedModel implements IBakedModel, IPartBakedMode
 
 	private List<BakedQuad> getQuadsForFrequency( final short frequency, final boolean active )
 	{
-		final List<BakedQuad> out = new ArrayList<>();
 		final AEColor[] colors = Platform.p2p().toColors( frequency );
+		final CubeBuilder cb = new CubeBuilder( this.format );
+
+		cb.setTexture( this.texture );
+		cb.useStandardUV();
+		cb.setRenderFullBright( active );
+
 		for( int i = 0; i < 4; ++i )
 		{
 			final int[] offs = QUAD_OFFSETS[i];
-			final CubeBuilder cb = new CubeBuilder( this.format );
+			for( int j = 0; j < 4; ++j )
+			{
+				final AEColor c = colors[j];
+				if( active )
+				{
+					cb.setColorRGB( c.dye.getColorValue() );
+				}
+				else
+				{
+					final float cv[] = c.dye.getColorComponentValues();
+					cb.setColorRGB( cv[0] * 0.5f, cv[1] * 0.5f, cv[2] * 0.5f );
+				}
 
-			cb.setTexture( this.texture );
-			cb.useStandardUV();
-			if( active )
-			{
-				cb.setRenderFullBright( true );
-				cb.setColorRGB( colors[i].dye.getColorValue() );
+				final int startx = j % 2;
+				final int starty = j / 2;
+
+				cb.addCube( offs[0] + startx, offs[1] + starty, offs[2], offs[0] + startx + 1, offs[1] + starty + 1, offs[2] + 1 );
 			}
-			else
-			{
-				final float cv[] = colors[i].dye.getColorComponentValues();
-				cb.setColorRGB( cv[0] * 0.5f, cv[1] * 0.5f, cv[2] * 0.5f );
-			}
-			cb.addCube( offs[0], offs[1], offs[2], offs[0] + 2, offs[1] + 2, offs[2] + 1 );
-			out.addAll( cb.getOutput() );
+
 		}
-		return out;
+		return cb.getOutput();
 	}
 
 	@Override
