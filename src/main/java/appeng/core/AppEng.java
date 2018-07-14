d5372a5ff694f8fb340ce61fed3edcb1607b6331
yueh <yueh@users.noreply.github.com>
2018-07-14 21:48:52 +0200

    Added fingerprint

--- src/main/java/appeng/core/AppEng.java
+++ src/main/java/appeng/core/AppEng.java
@@ -67,7 +67,7 @@ import appeng.services.version.VersionCheckerConfig;
 import appeng.util.Platform;
 
 
-@Mod( modid = AppEng.MOD_ID, acceptedMinecraftVersions = "[1.12.2]", name = AppEng.MOD_NAME, version = AEConfig.VERSION, dependencies = AppEng.MOD_DEPENDENCIES, guiFactory = "appeng.client.gui.config.AEConfigGuiFactory" )
+@Mod( modid = AppEng.MOD_ID, acceptedMinecraftVersions = "[1.12.2]", name = AppEng.MOD_NAME, version = AEConfig.VERSION, dependencies = AppEng.MOD_DEPENDENCIES, guiFactory = "appeng.client.gui.config.AEConfigGuiFactory", certificateFingerprint = "dfa4d3ac143316c6f32aa1a1beda1e34d42132e5" )
 public final class AppEng
 {
 	@SidedProxy( clientSide = "appeng.client.ClientHelper", serverSide = "appeng.server.ServerHelper", modId = AppEng.MOD_ID )
@@ -78,17 +78,7 @@ public final class AppEng
 
 	public static final String ASSETS = "appliedenergistics2:";
 
-	public static final String MOD_DEPENDENCIES =
-			// a few mods, AE should load after, probably.
-			// required-after:AppliedEnergistics2API|all;
-			// "after:gregtech_addon;after:Mekanism;after:IC2;after:ThermalExpansion;after:BuildCraft|Core;" +
-
-			// depend on version of forge used for build.
-			"after:appliedenergistics2-core;";// + "required-after:Forge@[" // require forge.
-	// + net.minecraftforge.common.ForgeVersion.majorVersion + '.' // majorVersion
-	// + net.minecraftforge.common.ForgeVersion.minorVersion + '.' // minorVersion
-	// + net.minecraftforge.common.ForgeVersion.revisionVersion + '.' // revisionVersion
-	// + net.minecraftforge.common.ForgeVersion.buildVersion + ",)"; // buildVersion
+	public static final String MOD_DEPENDENCIES = "";
 
 	@Nonnull
 	private static final AppEng INSTANCE = new AppEng();
