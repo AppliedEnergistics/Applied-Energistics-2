9cd89dd2a2d57e639f13ca4dc95f8eec1cf1cec5
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-06-02 05:10:26 +0100

    Add block loot generation.

--- src/main/java/appeng/data/providers/loot/IAE2LootProvider.java
+++ src/main/java/appeng/data/providers/loot/IAE2LootProvider.java
@@ -1,23 +1,23 @@
 package appeng.data.providers.loot;
 
 
-import appeng.data.providers.AE2DataProviderBase;
+import appeng.data.providers.IAE2DataProvider;
 import net.minecraft.world.storage.loot.LootParameterSet;
 import net.minecraft.world.storage.loot.LootTable;
 
 import javax.annotation.Nonnull;
 
 
-public abstract class AE2LootProviderBase extends AE2DataProviderBase
+public interface IAE2LootProvider extends IAE2DataProvider
 {
 
 	@Nonnull
 	@Override
-	protected LootTable finishBuilding( LootTable.Builder builder )
+	default LootTable finishBuilding( LootTable.Builder builder )
 	{
 		return builder.setParameterSet( getParameterSetTarget() ).build();
 	}
 
-	protected abstract LootParameterSet getParameterSetTarget();
+	LootParameterSet getParameterSetTarget();
 
 }
