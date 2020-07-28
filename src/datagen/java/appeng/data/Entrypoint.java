b8c63df11b6683f4b29bf9f3be0aac54c605e1fe
Sebastian Hartte <sebastian@hartte.de>
2020-07-28 23:16:08 +0200

    Make build depend on generateData, and remove unmapped mods from its dependencies.

--- src/datagen/java/appeng/data/Entrypoint.java
+++ src/datagen/java/appeng/data/Entrypoint.java
@@ -1,21 +1,19 @@
 package appeng.data;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.minecraft.Bootstrap;
-import net.minecraft.data.DataGenerator;
-
 import appeng.core.AEConfig;
 import appeng.core.Api;
 import appeng.core.CreativeTab;
 import appeng.data.providers.loot.BlockDropProvider;
 import appeng.data.providers.recipes.SlabStairRecipes;
 import appeng.data.providers.tags.ConventionTagProvider;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.Bootstrap;
+import net.minecraft.data.DataGenerator;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 public class Entrypoint implements PreLaunchEntrypoint {
 
@@ -31,6 +29,11 @@ public class Entrypoint implements PreLaunchEntrypoint {
 
     @Override
     public void onPreLaunch() {
+        if (!"true".equals(System.getProperty("appeng.generateData"))) {
+            System.out.println("Applied Energistics 2: Skipping data generation. System property not set.");
+            return;
+        }
+
         Bootstrap.initialize();
         AEConfig.load(new File("config"));
         CreativeTab.init(); // Vanilla data gens use item tab associations
