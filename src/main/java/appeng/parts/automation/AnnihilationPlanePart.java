27b8c5f3ac03c4ed102e32864bdc56b77e083690
shartte <shartte@users.noreply.github.com>
2020-08-05 22:39:04 +0200

    Plane Connection Refactor (#4556) (#4565)
    * Refactors all plane parts to use a common helper for
    calculating connections to adjacent planes.
    Also fixes problems with updating these connections on neighbor updates.
    
    * Fix conflicting GuiSync ID.
    
    * Slightly refactor PlaneConnectionHelper, and fix an issue where the bounding box was calculated incorrectly by using the wrong up-direction.
    
    (cherry picked from commit 8617dc8340ea1862662926562bc503260689303c)

--- src/main/java/appeng/parts/automation/AnnihilationPlanePart.java
+++ src/main/java/appeng/parts/automation/AnnihilationPlanePart.java
@@ -35,7 +35,6 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -58,9 +57,7 @@ import appeng.api.networking.storage.IStorageGrid;
 import appeng.api.networking.ticking.IGridTickable;
 import appeng.api.networking.ticking.TickRateModulation;
 import appeng.api.networking.ticking.TickingRequest;
-import appeng.api.parts.IPart;
 import appeng.api.parts.IPartCollisionHelper;
-import appeng.api.parts.IPartHost;
 import appeng.api.parts.IPartModel;
 import appeng.api.storage.channels.IItemStorageChannel;
 import appeng.api.storage.data.IAEItemStack;
@@ -96,12 +93,14 @@ public class AnnihilationPlanePart extends BasicStatePart implements IGridTickab
     private boolean isAccepting = true;
     private boolean breaking = false;
 
+    private final PlaneConnectionHelper connectionHelper = new PlaneConnectionHelper(this);
+
     public AnnihilationPlanePart(final ItemStack is) {
         super(is);
     }
 
     @Override
-    public TickRateModulation call(final World world) throws Exception {
+    public TickRateModulation call(final World world) {
         this.breaking = false;
         return this.breakBlock(true);
     }
@@ -117,39 +116,8 @@ public class AnnihilationPlanePart extends BasicStatePart implements IGridTickab
             return;
         }
 
-        int minX = 1;
-        int minY = 1;
-        int maxX = 15;
-        int maxY = 15;
+        connectionHelper.getBoxes(bch);
 
-        final IPartHost host = this.getHost();
-        if (host != null) {
-            final TileEntity te = host.getTile();
-
-            final BlockPos pos = te.getPos();
-
-            final Direction e = bch.getWorldX();
-            final Direction u = bch.getWorldY();
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(e.getOpposite())), this.getSide())) {
-                minX = 0;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(e)), this.getSide())) {
-                maxX = 16;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(u.getOpposite())), this.getSide())) {
-                minY = 0;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(e)), this.getSide())) {
-                maxY = 16;
-            }
-        }
-
-        bch.addBox(5, 5, 14, 11, 11, 15);
-        bch.addBox(minX, minY, 15, maxX, maxY, 16);
     }
 
     /**
@@ -157,73 +125,15 @@ public class AnnihilationPlanePart extends BasicStatePart implements IGridTickab
      *         visually.
      */
     public PlaneConnections getConnections() {
-
-        final Direction facingRight, facingUp;
-        AEPartLocation location = this.getSide();
-        switch (location) {
-            case UP:
-                facingRight = Direction.EAST;
-                facingUp = Direction.NORTH;
-                break;
-            case DOWN:
-                facingRight = Direction.WEST;
-                facingUp = Direction.NORTH;
-                break;
-            case NORTH:
-                facingRight = Direction.WEST;
-                facingUp = Direction.UP;
-                break;
-            case SOUTH:
-                facingRight = Direction.EAST;
-                facingUp = Direction.UP;
-                break;
-            case WEST:
-                facingRight = Direction.SOUTH;
-                facingUp = Direction.UP;
-                break;
-            case EAST:
-                facingRight = Direction.NORTH;
-                facingUp = Direction.UP;
-                break;
-            default:
-            case INTERNAL:
-                return PlaneConnections.of(false, false, false, false);
-        }
-
-        boolean left = false, right = false, down = false, up = false;
-
-        final IPartHost host = this.getHost();
-        if (host != null) {
-            final TileEntity te = host.getTile();
-
-            final BlockPos pos = te.getPos();
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(facingRight.getOpposite())),
-                    this.getSide())) {
-                left = true;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(facingRight)), this.getSide())) {
-                right = true;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(facingUp.getOpposite())),
-                    this.getSide())) {
-                down = true;
-            }
-
-            if (this.isAnnihilationPlane(te.getWorld().getTileEntity(pos.offset(facingUp)), this.getSide())) {
-                up = true;
-            }
-        }
-
-        return PlaneConnections.of(up, right, down, left);
+        return connectionHelper.getConnections();
     }
 
     @Override
     public void onNeighborChanged(IBlockReader w, BlockPos pos, BlockPos neighbor) {
         if (pos.offset(this.getSide().getFacing()).equals(neighbor)) {
             this.refresh();
+        } else {
+            connectionHelper.updateConnections();
         }
     }
 
@@ -367,14 +277,6 @@ public class AnnihilationPlanePart extends BasicStatePart implements IGridTickab
         entityItem.getItem().setCount(newStackSize);
 
         return changed;
-    }
-
-    protected boolean isAnnihilationPlane(final TileEntity blockTileEntity, final AEPartLocation side) {
-        if (blockTileEntity instanceof IPartHost) {
-            final IPart p = ((IPartHost) blockTileEntity).getPart(side);
-            return p != null && p.getClass() == this.getClass();
-        }
-        return false;
     }
 
     @Override
