f335eca6c71b386261d4f5683112520ee7041bfa
yueh <yueh@users.noreply.github.com>
2021-02-17 15:05:47 +0100

    Added information grid to query details about a grid
    Currently provides a list of worlds and chunk a grid spawns.

---
+++ src/main/java/appeng/me/cache/InformationCache.java
@@ -0,0 +1,164 @@
+/*
+ * This file is part of Applied Energistics 2.
+ * Copyright (c) 2013 - 2014, AlgorithmX2, All rights reserved.
+ *
+ * Applied Energistics 2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Applied Energistics 2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Applied Energistics 2.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package appeng.me.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.IWorld;
+
+import appeng.api.networking.IGrid;
+import appeng.api.networking.IGridCache;
+import appeng.api.networking.IGridHost;
+import appeng.api.networking.IGridNode;
+import appeng.api.networking.IGridStorage;
+import appeng.api.util.DimensionalCoord;
+
+public class InformationCache implements IGridCache {
+    private IGrid grid;
+    private Map<IWorld, Multiset<ChunkPos>> chunks;
+
+    public InformationCache(final IGrid g) {
+        this.grid = g;
+        this.chunks = new HashMap<>();
+    }
+
+    @Override
+    public void onUpdateTick() {
+//        System.out.println(this.myGrid);
+//
+//        this.worlds().forEach(w -> {
+//            System.out.println(w + " @ " + this.chunks(w));
+//        });
+//
+//        System.out.println("========");
+    }
+
+    @Override
+    public void removeNode(final IGridNode node, final IGridHost machine) {
+        if (node.getGridBlock().isWorldAccessible()) {
+            DimensionalCoord loc = node.getGridBlock().getLocation();
+
+            this.removeChunk(loc.getWorld(), loc.getBlockPos());
+        }
+    }
+
+    @Override
+    public void addNode(final IGridNode node, final IGridHost machine) {
+        if (node.getGridBlock().isWorldAccessible()) {
+            DimensionalCoord loc = node.getGridBlock().getLocation();
+
+            this.addChunk(loc.getWorld(), loc.getBlockPos());
+        }
+    }
+
+    @Override
+    public void onSplit(final IGridStorage storageB) {
+
+    }
+
+    @Override
+    public void onJoin(final IGridStorage storageB) {
+
+    }
+
+    @Override
+    public void populateGridStorage(final IGridStorage storage) {
+
+    }
+
+    public IGrid getGrid() {
+        return grid;
+    }
+
+    /**
+     * A set of all {@link IWorld} this grid spans.
+     * 
+     * @return
+     */
+    public Set<IWorld> worlds() {
+        return this.chunks.keySet();
+    }
+
+    /**
+     * A set of chunks this grid spans in a specific world.
+     * 
+     * @param world
+     * @return
+     */
+    public Set<ChunkPos> chunks(IWorld world) {
+        return this.chunks.get(world).elementSet();
+    }
+
+    public Map<IWorld, Multiset<ChunkPos>> getChunks() {
+        return this.chunks;
+    }
+
+    /**
+     * Mark the chunk of the {@link BlockPos} as location of the network.
+     * 
+     * @param world
+     * @param pos
+     * @return
+     */
+    private boolean addChunk(IWorld world, BlockPos pos) {
+        return this.getChunks(world).add(new ChunkPos(pos));
+    }
+
+    /**
+     * Remove the chunk of this {@link BlockPos} from the network locations.
+     * 
+     * This uses a {@link Multiset} to ensure it will only marked as no longer containing a grid once all other
+     * gridnodes are removed as well.
+     * 
+     * @param world
+     * @param pos
+     * @return
+     */
+    private boolean removeChunk(IWorld world, BlockPos pos) {
+        boolean ret = this.getChunks(world).remove(new ChunkPos(pos));
+
+        this.clearWorld(world);
+
+        return ret;
+    }
+
+    private Multiset<ChunkPos> getChunks(IWorld world) {
+        return this.chunks.computeIfAbsent(world, w -> {
+            return HashMultiset.create();
+        });
+    }
+
+    /**
+     * Cleanup the map in case a whole world is unloaded
+     * 
+     * @param world
+     */
+    private void clearWorld(IWorld world) {
+        if (this.chunks.get(world).isEmpty()) {
+            this.chunks.remove(world);
+        }
+    }
+}
