a20c5f96fb54ff93463e307b9b700daeeb414039
adde0109 <52883522+adde0109@users.noreply.github.com>
2021-05-06 19:55:17 +0200

    Fix "Incomplete server tags, disconnecting" (#5194)

--- src/main/java/appeng/items/parts/FacadeItem.java
+++ src/main/java/appeng/items/parts/FacadeItem.java
@@ -59,7 +59,7 @@ public class FacadeItem extends AEBaseItem implements IFacadeItem, IAlphaPassIte
      * Block tag used to explicitly whitelist blocks for use in facades.
      */
     private static final ITag.INamedTag<Block> BLOCK_WHITELIST = BlockTags
-            .makeWrapperTag(AppEng.makeId("whitelisted/facades").toString());
+            .createOptional(AppEng.makeId("whitelisted/facades"));
 
     private static final String NBT_ITEM_ID = "item";
 
