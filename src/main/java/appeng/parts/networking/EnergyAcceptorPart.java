bef8cbf78aab2d2ebca08d94f4fd9925841101fc
yueh <yueh@users.noreply.github.com>
2021-05-18 17:16:06 +0200

    Fixes #785: Adds a part variant of the energy acceptor
    As Forge now moved to pretty much FE only we can add a part variant of
    the energy acceptor without causing various problems by directly
    exposing a particular energy API on every single cable.
    
    This also means we most likely will no longer be able to support any non
    capability based energy API should they arise as this will cause a
    mismatch between what the block and part variants can handle.

---
+++ src/main/java/appeng/parts/networking/EnergyAcceptorPart.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of Applied Energistics 2.
+ * Copyright (c) 2021, TeamAppliedEnergistics, All rights reserved.
+ *
+ * Applied Energistics 2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Applied Energistics 2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Applied Energistics 2.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package appeng.parts.networking;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+
+import appeng.api.config.AccessRestriction;
+import appeng.api.config.Actionable;
+import appeng.api.config.PowerMultiplier;
+import appeng.api.config.PowerUnits;
+import appeng.api.networking.energy.IEnergyGrid;
+import appeng.api.parts.IPartCollisionHelper;
+import appeng.api.parts.IPartModel;
+import appeng.api.util.AECableType;
+import appeng.api.util.AEPartLocation;
+import appeng.capabilities.Capabilities;
+import appeng.core.AppEng;
+import appeng.helpers.ForgeEnergyAdapter;
+import appeng.items.parts.PartModels;
+import appeng.me.GridAccessException;
+import appeng.parts.AEBasePart;
+import appeng.parts.PartModel;
+import appeng.tile.powersink.IExternalPowerSink;
+
+public class EnergyAcceptorPart extends AEBasePart implements IExternalPowerSink {
+
+    @PartModels
+    private static final IPartModel MODELS = new PartModel(new ResourceLocation(AppEng.MOD_ID, "part/energy_acceptor"));
+    private ForgeEnergyAdapter forgeEnergyAdapter;
+    private LazyOptional<ForgeEnergyAdapter> forgeEnergyAdapterOptional;
+
+    public EnergyAcceptorPart(final ItemStack is) {
+        super(is);
+        this.getProxy().setIdlePowerUsage(0);
+        this.forgeEnergyAdapter = new ForgeEnergyAdapter(this);
+        this.forgeEnergyAdapterOptional = LazyOptional.of(() -> forgeEnergyAdapter);
+    }
+
+    @Override
+    @Nonnull
+    public <T> LazyOptional<T> getCapability(Capability<T> capability) {
+        if (capability == Capabilities.FORGE_ENERGY) {
+            return (LazyOptional<T>) this.forgeEnergyAdapterOptional;
+        }
+
+        return super.getCapability(capability);
+    }
+
+    @Override
+    public AECableType getCableConnectionType(final AEPartLocation dir) {
+        return AECableType.GLASS;
+    }
+
+    @Override
+    public void getBoxes(final IPartCollisionHelper bch) {
+        bch.addBox(2, 2, 14, 14, 14, 16);
+        bch.addBox(4, 4, 12, 12, 12, 14);
+    }
+
+    @Override
+    public float getCableConnectionLength(AECableType cable) {
+        return 2;
+    }
+
+    @Override
+    public IPartModel getStaticModels() {
+        return MODELS;
+    }
+
+    @Override
+    public final double getExternalPowerDemand(final PowerUnits externalUnit, final double maxPowerRequired) {
+        return PowerUnits.AE.convertTo(externalUnit,
+                Math.max(0.0, this.getFunnelPowerDemand(externalUnit.convertTo(PowerUnits.AE, maxPowerRequired))));
+    }
+
+    protected double getFunnelPowerDemand(final double maxRequired) {
+        try {
+            final IEnergyGrid grid = this.getProxy().getEnergy();
+
+            return grid.getEnergyDemand(maxRequired);
+        } catch (final GridAccessException e) {
+            return 0;
+        }
+    }
+
+    @Override
+    public final double injectExternalPower(final PowerUnits input, final double amt, Actionable mode) {
+        return PowerUnits.AE.convertTo(input, this.funnelPowerIntoStorage(input.convertTo(PowerUnits.AE, amt), mode));
+    }
+
+    protected double funnelPowerIntoStorage(final double power, final Actionable mode) {
+        try {
+            final IEnergyGrid grid = this.getProxy().getEnergy();
+            final double leftOver = grid.injectPower(power, mode);
+
+            return leftOver;
+        } catch (final GridAccessException e) {
+            return power;
+        }
+    }
+
+    @Override
+    public final double injectAEPower(double amt, final Actionable mode) {
+        return amt;
+    }
+
+    @Override
+    public final double getAEMaxPower() {
+        return 0;
+    }
+
+    @Override
+    public final double getAECurrentPower() {
+        return 0;
+    }
+
+    @Override
+    public final boolean isAEPublicPowerStorage() {
+        return false;
+    }
+
+    @Override
+    public final AccessRestriction getPowerFlow() {
+        return AccessRestriction.READ_WRITE;
+    }
+
+    @Override
+    public final double extractAEPower(final double amt, final Actionable mode, final PowerMultiplier multiplier) {
+        return 0;
+    }
+}
