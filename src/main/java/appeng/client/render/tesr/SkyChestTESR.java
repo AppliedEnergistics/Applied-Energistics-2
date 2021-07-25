36be9b1391d3c1882eeadb67c836336880aa09d1
Sebastian Hartte <sebastian@hartte.de>
2021-07-25 02:09:04 +0200

    Ported sky chest rendering to new system for model parts.

--- src/main/java/appeng/client/render/tesr/SkyChestTESR.java
+++ src/main/java/appeng/client/render/tesr/SkyChestTESR.java
@@ -18,52 +18,61 @@
 
 package appeng.client.render.tesr;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-
-import com.mojang.math.Vector3f;
-import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.resources.model.Material;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.TextureStitchEvent;
-
 import appeng.block.storage.SkyChestBlock;
 import appeng.block.storage.SkyChestBlock.SkyChestType;
 import appeng.core.AppEng;
 import appeng.tile.storage.SkyChestTileEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.TextureStitchEvent;
 
 // This is mostly a copy&paste job of the vanilla chest TESR
 @OnlyIn(Dist.CLIENT)
 public class SkyChestTESR implements BlockEntityRenderer<SkyChestTileEntity> {
+
+    public static ModelLayerLocation MODEL_LAYER = new ModelLayerLocation(AppEng.makeId("sky_chest"), "main");
 
     public static final Material TEXTURE_STONE = new Material(Sheets.CHEST_SHEET,
             new ResourceLocation(AppEng.MOD_ID, "models/skychest"));
     public static final Material TEXTURE_BLOCK = new Material(Sheets.CHEST_SHEET,
             new ResourceLocation(AppEng.MOD_ID, "models/skyblockchest"));
 
-    private final ModelPart singleLid;
-    private final ModelPart singleBottom;
-    private final ModelPart singleLatch;
+    private final ModelPart lid;
+    private final ModelPart bottom;
+    private final ModelPart lock;
 
     public SkyChestTESR(BlockEntityRendererProvider.Context context) {
-        this.singleBottom = new ModelPart(64, 64, 0, 19);
-        this.singleBottom.addBox(1.0F, 0.0F, 1.0F, 14.0F, 10.0F, 14.0F, 0.0F);
-        this.singleLid = new ModelPart(64, 64, 0, 0);
-        this.singleLid.addBox(1.0F, 0.0F, 0.0F, 14.0F, 5.0F, 14.0F, 0.0F);
-        this.singleLid.y = 10.0F;
-        this.singleLid.z = 1.0F;
-        this.singleLatch = new ModelPart(64, 64, 0, 0);
-        this.singleLatch.addBox(7.0F, -1.0F, 15.0F, 2.0F, 4.0F, 1.0F, 0.0F);
-        this.singleLatch.y = 9.0F;
+        var modelpart = context.bakeLayer(MODEL_LAYER);
+        this.bottom = modelpart.getChild("bottom");
+        this.lid = modelpart.getChild("lid");
+        this.lock = modelpart.getChild("lock");
+    }
+
+    public static LayerDefinition createSingleBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        partdefinition.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(0, 19).addBox(1.0F, 0.0F, 1.0F, 14.0F, 10.0F, 14.0F), PartPose.ZERO);
+        partdefinition.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(0, 0).addBox(1.0F, 0.0F, 0.0F, 14.0F, 5.0F, 14.0F), PartPose.offset(0.0F, 10.0F, 1.0F));
+        partdefinition.addOrReplaceChild("lock", CubeListBuilder.create().texOffs(0, 0).addBox(7.0F, -1.0F, 15.0F, 2.0F, 4.0F, 1.0F), PartPose.offset(0.0F, 9.0F, 0.0F));
+        return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
     @Override
@@ -80,7 +89,7 @@ public class SkyChestTESR implements BlockEntityRenderer<SkyChestTileEntity> {
         f1 = 1.0F - f1 * f1 * f1;
         Material material = this.getRenderMaterial(tileEntityIn);
         VertexConsumer ivertexbuilder = material.buffer(bufferIn, RenderType::entityCutout);
-        this.renderModels(matrixStackIn, ivertexbuilder, this.singleLid, this.singleLatch, this.singleBottom, f1,
+        this.renderModels(matrixStackIn, ivertexbuilder, this.lid, this.lock, this.bottom, f1,
                 combinedLightIn, combinedOverlayIn);
 
         matrixStackIn.popPose();
