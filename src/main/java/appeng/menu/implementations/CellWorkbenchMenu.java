0480ee3dbfac87a23a9f50259fbb897e469cbd37
Sebastian Hartte <sebastian@hartte.de>
2021-08-21 22:55:30 +0200

    Fix an issue in CellUpgrades that caused the upgrade inventories cached upgrade counts to not be re-calculated when cards were being removed.

--- src/main/java/appeng/menu/implementations/CellWorkbenchMenu.java
+++ src/main/java/appeng/menu/implementations/CellWorkbenchMenu.java
@@ -59,8 +59,6 @@ public class CellWorkbenchMenu extends UpgradeableMenu {
     private final CellWorkbenchBlockEntity workBench;
     @GuiSync(2)
     public CopyMode copyMode = CopyMode.CLEAR_ON_REMOVE;
-    private ItemStack prevStack = ItemStack.EMPTY;
-    private int lastUpgrades = 0;
 
     public CellWorkbenchMenu(int id, final Inventory ip, final CellWorkbenchBlockEntity te) {
         super(TYPE, id, ip, te);
@@ -117,7 +115,6 @@ public class CellWorkbenchMenu extends UpgradeableMenu {
 
     @Override
     public void broadcastChanges() {
-        final ItemStack is = getWorkbenchItem();
         if (isServer()) {
             this.setCopyMode(this.getWorkBenchCopyMode());
             this.setFuzzyMode(this.getWorkBenchFuzzyMode());
