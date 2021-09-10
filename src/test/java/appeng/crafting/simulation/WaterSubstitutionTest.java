3a72fc1d925bd9dd16b2594801fc99fece8746e0
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-09-10 13:18:32 +0200

    Test substitutions with a template of amount > 1

--- src/test/java/appeng/crafting/simulation/WaterSubstitutionTest.java
+++ src/test/java/appeng/crafting/simulation/WaterSubstitutionTest.java
@@ -10,7 +10,6 @@ import appeng.util.BootstrapMinecraft;
 import appeng.util.item.AEItemStack;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -30,7 +29,6 @@ public class WaterSubstitutionTest {
 	public void testWaterSubstitution() {
 		var env = new SimulationEnv();
 
-		IAEItemStack bucket = AEItemStack.fromItemStack(new ItemStack(Items.BUCKET));
 		IAEItemStack waterBucket = AEItemStack.fromItemStack(new ItemStack(Items.WATER_BUCKET));
 		IAEItemStack dirt = AEItemStack.fromItemStack(new ItemStack(Items.DIRT));
 		IAEItemStack grass = AEItemStack.fromItemStack(new ItemStack(Items.GRASS));
@@ -38,40 +36,33 @@ public class WaterSubstitutionTest {
 		IAEItemStack water1mb = AEItemStack.fromItemStack(new ItemStack(Items.DIAMOND));
 		IAEItemStack water1000mb = water1mb.copyWithStackSize(1000);
 
-		// 1 dirt + 2 water buckets -> 1 grass.
+		// 1 dirt + 2x [1000 water or 1 water bucket] -> 1 grass.
 		var grassPattern = env.addPattern(new ProcessingPatternBuilder(grass)
 				.addStrictInput(1, dirt)
-				.addStrictInput(2, waterBucket)
-				.build()
-		);
-		// 1 empty bucket + 1000 water -> 1 water bucket.
-		var waterBucketPattern = env.addPattern(new ProcessingPatternBuilder(waterBucket)
-				.addStrictInput(1, bucket)
-				.addStrictInput(1, water1000mb)
+				.addStrictInput(2, water1000mb, waterBucket)
 				.build()
 		);
 
-		// Dirt, water and empty buckets are infinite.
+		// Dirt is infinite.
 		env.setEmitable(dirt);
-		env.setEmitable(bucket);
-		env.setEmitable(water1mb);
 
-		// Let's add 1 stored water bucket.
+		// Let's add 1 stored water bucket and 3500 mb.
+		// We add a little bit more water to test that exact multiples of the template get extracted.
 		env.addStoredItem(waterBucket);
+		env.addStoredItem(water1000mb.copyWithStackSize(3500));
 
-		// Crafting should use the water bucket from the network, and craft 3.
+		// Crafting should use the water bucket from the network and then the water directly.
 		var plan = env.doCrafting(grass.copyWithStackSize(2));
 
 		assertThat(plan.simulation()).isFalse();
 		assertThat(plan.missingItems()).isEmpty();
 
-		assertThat(plan.patternTimes()).isEqualTo(Map.of(grassPattern, 2L, waterBucketPattern, 3L));
+		assertThat(plan.patternTimes()).isEqualTo(Map.of(grassPattern, 2L));
 
 		assertStackSize(plan.emittedItems(), dirt, 2);
-		assertStackSize(plan.emittedItems(), bucket, 3);
-		assertStackSize(plan.emittedItems(), water1mb, 3000);
 
 		assertStackSize(plan.usedItems(), waterBucket, 1);
+		assertStackSize(plan.usedItems(), water1mb, 3000);
 	}
 
 	private static <T extends IAEStack<T>> void assertStackSize(IItemList<T> list, T what, long count) {
