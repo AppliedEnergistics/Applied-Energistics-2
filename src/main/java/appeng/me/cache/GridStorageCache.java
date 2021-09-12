9335daff7d3efc76c3c75d243ba973a25c299791
PrototypeTrousers <salomaoo@gmail.com>
2021-09-11 22:38:26 -0300

    Revert "fixes item showing incorrect amounts"
    This reverts commit 618e9390c6929ecfceb89be4fb552d8c222fd517.

--- src/main/java/appeng/me/cache/GridStorageCache.java
+++ src/main/java/appeng/me/cache/GridStorageCache.java
@@ -249,6 +249,8 @@ public class GridStorageCache implements IStorageGrid
 		}
 
 		tracker.applyChanges();
+
+		this.storageMonitors.forEach( ( channel, monitor ) -> monitor.forceUpdate() );
 	}
 
 	private <T extends IAEStack<T>, C extends IStorageChannel<T>> void postChangesToNetwork( final C chan, final int upOrDown, final IItemList<T> availableItems, final IActionSource src )
