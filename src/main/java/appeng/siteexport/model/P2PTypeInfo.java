6ca01f3d94a8f93bd957f671e696d51c2842b38d
Sebastian Hartte <shartte@users.noreply.github.com>
2021-10-31 17:34:04 +0100

    Added export of game-data for new website as a client-side command (/ae2export) that is only available in a development environment.

---
+++ src/main/java/appeng/siteexport/model/P2PTypeInfo.java
@@ -0,0 +1,18 @@
+package appeng.siteexport.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Describes a type of P2P tunnel.
+ */
+public class P2PTypeInfo {
+    // ID of the tunnel part that represents this type
+    public String tunnelItemId;
+    // IDs of items that cause attunement to this type
+    public List<String> attunementItemIds = new ArrayList<>();
+    // Mod IDs that cause attunement to this type
+    public List<String> attunementModIds = new ArrayList<>();
+    // Class-Names of APIs provided by items that cause attunement to this type
+    public List<String> attunementApiClasses = new ArrayList<>();
+}
