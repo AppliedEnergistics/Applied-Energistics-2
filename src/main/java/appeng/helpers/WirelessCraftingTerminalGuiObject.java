d960cbcf4e4817de50df777126b7fc5d162223fe
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-11-17 09:52:58 +0100

    run spotless

--- src/main/java/appeng/helpers/WirelessCraftingTerminalGuiObject.java
+++ src/main/java/appeng/helpers/WirelessCraftingTerminalGuiObject.java
@@ -1,16 +1,18 @@
 package appeng.helpers;
 
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
 import appeng.api.features.IWirelessTerminalHandler;
 import appeng.api.inventories.ISegmentedInventory;
 import appeng.api.inventories.InternalInventory;
 import appeng.parts.reporting.CraftingTerminalPart;
 import appeng.util.inv.AppEngInternalInventory;
 import appeng.util.inv.InternalInventoryHost;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 
 public class WirelessCraftingTerminalGuiObject extends WirelessTerminalGuiObject
         implements ISegmentedInventory, InternalInventoryHost {
