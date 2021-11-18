15a61e3e7c35bae93403abf2c1e565b7395adf39
Sebastian Hartte <shartte@users.noreply.github.com>
2021-11-18 21:36:59 +0100

    First rename pass for appliedenergistics2->ae2

--- src/main/java/appeng/datagen/providers/models/BlockModelProvider.java
+++ src/main/java/appeng/datagen/providers/models/BlockModelProvider.java
@@ -1,9 +1,23 @@
 package appeng.datagen.providers.models;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.data.models.blockstates.PropertyDispatch;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.blockstates.VariantProperties;
+import net.minecraft.data.models.model.TextureMapping;
+import net.minecraft.data.models.model.TextureSlot;
+import net.minecraft.data.models.model.TexturedModel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.WallSide;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
+import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import appeng.core.AppEng;
@@ -26,7 +40,7 @@ public class BlockModelProvider extends BlockStateProvider implements IAE2DataPr
     /**
      * Generate an ore block with 4 variants (0 to 3, inclusive).
      */
-    private void generateOreBlock(BlockDefinition<QuartzOreBlock> block) {
+    private void generateOreBlock(BlockDefinition<?> block) {
         String name = block.id().getPath();
         BlockModelBuilder primaryModel = models().cubeAll(
                 name + "_0",
