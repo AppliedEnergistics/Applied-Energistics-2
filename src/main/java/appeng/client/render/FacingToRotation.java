52070944242b409ed42e7ab8b12f4ab32897d927
Sebastian Hartte <shartte@users.noreply.github.com>
2021-11-20 23:23:56 +0100

    Fix #4572: Correct vertex winding after rotating a quad.

--- src/main/java/appeng/client/render/FacingToRotation.java
+++ src/main/java/appeng/client/render/FacingToRotation.java
@@ -37,25 +37,42 @@ public enum FacingToRotation implements StringRepresentable {
 
     // DUNSWE
     // @formatter:off
-    DOWN_DOWN(new Vector3f(0, 0, 0)), // NOOP
-    DOWN_UP(new Vector3f(0, 0, 0)), // NOOP
-    DOWN_NORTH(new Vector3f(-90, 0, 0)), DOWN_SOUTH(new Vector3f(-90, 0, 180)), DOWN_WEST(new Vector3f(-90, 0, 90)),
-    DOWN_EAST(new Vector3f(-90, 0, -90)), UP_DOWN(new Vector3f(0, 0, 0)), // NOOP
-    UP_UP(new Vector3f(0, 0, 0)), // NOOP
-    UP_NORTH(new Vector3f(90, 0, 180)), UP_SOUTH(new Vector3f(90, 0, 0)), UP_WEST(new Vector3f(90, 0, 90)),
-    UP_EAST(new Vector3f(90, 0, -90)), NORTH_DOWN(new Vector3f(0, 0, 180)), NORTH_UP(new Vector3f(0, 0, 0)),
-    NORTH_NORTH(new Vector3f(0, 0, 0)), // NOOP
-    NORTH_SOUTH(new Vector3f(0, 0, 0)), // NOOP
-    NORTH_WEST(new Vector3f(0, 0, 90)), NORTH_EAST(new Vector3f(0, 0, -90)), SOUTH_DOWN(new Vector3f(0, 180, 180)),
-    SOUTH_UP(new Vector3f(0, 180, 0)), SOUTH_NORTH(new Vector3f(0, 0, 0)), // NOOP
-    SOUTH_SOUTH(new Vector3f(0, 0, 0)), // NOOP
-    SOUTH_WEST(new Vector3f(0, 180, -90)), SOUTH_EAST(new Vector3f(0, 180, 90)), WEST_DOWN(new Vector3f(0, 90, 180)),
-    WEST_UP(new Vector3f(0, 90, 0)), WEST_NORTH(new Vector3f(0, 90, -90)), WEST_SOUTH(new Vector3f(0, 90, 90)),
-    WEST_WEST(new Vector3f(0, 0, 0)), // NOOP
-    WEST_EAST(new Vector3f(0, 0, 0)), // NOOP
-    EAST_DOWN(new Vector3f(0, -90, 180)), EAST_UP(new Vector3f(0, -90, 0)), EAST_NORTH(new Vector3f(0, -90, 90)),
-    EAST_SOUTH(new Vector3f(0, -90, -90)), EAST_WEST(new Vector3f(0, 0, 0)), // NOOP
-    EAST_EAST(new Vector3f(0, 0, 0)); // NOOP
+    DOWN_DOWN(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    DOWN_UP(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    DOWN_NORTH(new Vector3f(-90, 0, 0)),
+    DOWN_SOUTH(new Vector3f(-90, 0, 180)),
+    DOWN_WEST(new Vector3f(-90, 0, 90)),
+    DOWN_EAST(new Vector3f(-90, 0, -90)),
+    UP_DOWN(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    UP_UP(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    UP_NORTH(new Vector3f(90, 0, 180)),
+    UP_SOUTH(new Vector3f(90, 0, 0)),
+    UP_WEST(new Vector3f(90, 0, 90)),
+    UP_EAST(new Vector3f(90, 0, -90)),
+    NORTH_DOWN(new Vector3f(0, 0, 180)),
+    NORTH_UP(new Vector3f(0, 0, 0)), // Default
+    NORTH_NORTH(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    NORTH_SOUTH(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    NORTH_WEST(new Vector3f(0, 0, 90)),
+    NORTH_EAST(new Vector3f(0, 0, -90)),
+    SOUTH_DOWN(new Vector3f(0, 180, 180)),
+    SOUTH_UP(new Vector3f(0, 180, 0)),
+    SOUTH_NORTH(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    SOUTH_SOUTH(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    SOUTH_WEST(new Vector3f(0, 180, -90)),
+    SOUTH_EAST(new Vector3f(0, 180, 90)),
+    WEST_DOWN(new Vector3f(0, 90, 180)),
+    WEST_UP(new Vector3f(0, 90, 0)),
+    WEST_NORTH(new Vector3f(0, 90, -90)),
+    WEST_SOUTH(new Vector3f(0, 90, 90)),
+    WEST_WEST(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    WEST_EAST(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    EAST_DOWN(new Vector3f(0, -90, 180)),
+    EAST_UP(new Vector3f(0, -90, 0)),
+    EAST_NORTH(new Vector3f(0, -90, 90)),
+    EAST_SOUTH(new Vector3f(0, -90, -90)),
+    EAST_WEST(new Vector3f(0, 0, 0)), // Invalid, NOOP
+    EAST_EAST(new Vector3f(0, 0, 0)); // Invalid, NOOP
     // @formatter:on
 
     private final Vector3f rot;
