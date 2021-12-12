66a2aebd8490c8b331d653609ca3b7bf3a36db64
Sebastian Hartte <shartte@users.noreply.github.com>
2021-12-12 21:31:01 +0100

    Fix network booting notifying the node before the network actually enters booting state (which causes many blocks to not change visually). This also changes that the state of "booting" only depends on the boolean flag and not on there being any active nodes still being pathed. Otherwise the notification cannot be sent before nodes are actually put into that set.

---
+++ src/test/java/appeng/me/GridNodeTest.java
@@ -0,0 +1,36 @@
+package appeng.me;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.reset;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import appeng.api.networking.IGridNodeListener;
+import appeng.me.service.PathingService;
+
+class GridNodeTest extends AbstractGridNodeTest {
+    /**
+     * Regression test for the {@link appeng.integration.modules.waila.GridNodeState#NETWORK_BOOTING} notification. It
+     * was previously sent before the state actually changed, causing various problems.
+     */
+    @Test
+    public void rebootNotificationIsPostedAfterGridBeginsBooting() {
+        var node = makePoweredNode();
+        assertTrue(node.hasGridBooted());
+        reset(listener);
+        var calls = new ArrayList<Boolean>();
+        doAnswer(invocation -> {
+            calls.add(node.hasGridBooted());
+            return null;
+        }).when(listener).onStateChanged(owner, node, IGridNodeListener.State.GRID_BOOT);
+        var pathingService = (PathingService) node.getGrid().getPathingService();
+        pathingService.repath();
+        runTick(node.getGrid());
+        assertThat(calls).containsOnly(false);
+    }
+
+}
