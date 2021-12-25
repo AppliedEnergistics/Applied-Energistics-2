09418bb1cd40f6b672cc0f8cfe0f4cd510d1dc99
Azercoco <20425137+Azercoco@users.noreply.github.com>
2021-12-25 23:48:55 +0100

    Add Insertion in Portable Cells with Right-Click

--- src/main/java/appeng/helpers/FluidContainerHelper.java
+++ src/main/java/appeng/helpers/FluidContainerHelper.java
@@ -5,8 +5,13 @@ import javax.annotation.Nullable;
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 import appeng.api.stacks.AEFluidKey;
@@ -39,5 +44,39 @@ public final class FluidContainerHelper {
         }
 
         return ContainerItemContext.withInitial(stack).find(FluidStorage.ITEM);
+    }
+
+    public static void extractFromPlayerCursorSlot(Player player, AEFluidKey key, long amount) {
+        try (Transaction tx = Transaction.openOuter()) {
+            ContainerItemContext.ofPlayerCursor(player, player.containerMenu).find(FluidStorage.ITEM).extract(
+                    key.toVariant(), amount, tx);
+            tx.commit();
+        }
+    }
+
+    public static boolean extractFromPlayerStack(Player player, AEFluidKey key, long amount, ItemStack stack) {
+        try (Transaction tx = Transaction.openOuter()) {
+            Inventory inventory = player.getInventory();
+            ContainerItemContext context = null;
+
+            for (int i = 0; i < inventory.getContainerSize(); ++i) {
+                if (inventory.getItem(i) == stack) {
+                    InventoryStorage wrapper = PlayerInventoryStorage.of(inventory);
+                    context = ContainerItemContext.ofPlayerSlot(player, wrapper.getSlots().get(i));
+                    break;
+                }
+            }
+
+            if (context != null) {
+                boolean ok = context.find(FluidStorage.ITEM).extract(
+                        key.toVariant(), amount, tx) == amount;
+                if (ok) {
+                    tx.commit();
+                    return true;
+                }
+
+            }
+            return false;
+        }
     }
 }
