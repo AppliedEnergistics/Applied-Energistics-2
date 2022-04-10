c0cc0112288cc267c2318caa0111e2e5761e563e
Mari023 <38946771+Mari023@users.noreply.github.com>
2022-04-10 21:11:19 +0200

    rework SecurityFiber to directly call the security terminal instead

--- src/main/java/appeng/parts/networking/SecurityFiberPart.java
+++ src/main/java/appeng/parts/networking/SecurityFiberPart.java
@@ -3,53 +3,69 @@ package appeng.parts.networking;
 import java.util.EnumSet;
 import java.util.Map;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
 import appeng.api.config.SecurityPermissions;
+import appeng.api.features.Locatables;
 import appeng.api.networking.IManagedGridNode;
 import appeng.api.networking.security.ISecurityProvider;
 import appeng.api.parts.IPartItem;
+import appeng.blockentity.misc.SecurityStationBlockEntity;
 import appeng.me.service.SecurityService;
 
 public class SecurityFiberPart extends QuartzFiberPart {
     public SecurityFiberPart(IPartItem<?> partItem) {
         super(partItem);
-        if (isSecurityServiceAbsent(outerNode))
-            outerNode.addService(ISecurityProvider.class, new SecurityBridge(getMainNode()));
-        else if (isSecurityServiceAbsent(getMainNode()))
-            getMainNode().addService(ISecurityProvider.class, new SecurityBridge(outerNode));
     }
 
-    private static SecurityService getSecurityService(IManagedGridNode node) {
-        if (node.getGrid() == null)
-            return null;
-        if (!(node.getGrid().getSecurityService() instanceof SecurityService securityService))
-            return null;
-        return securityService;
+    @Override
+    public void onPlacement(Player player) {
+        super.onPlacement(player);
+        connectSecurity(player.getLevel());
     }
 
-    private static boolean isSecurityServiceAbsent(IManagedGridNode node) {
-        SecurityService securityService = getSecurityService(node);
-        if (securityService == null)
-            return true;
-        return !securityService.isAvailable();
+    private void connectSecurity(Level level) {
+        connectSecurity(getMainNode(), outerNode, level);
+        connectSecurity(outerNode, getMainNode(), level);
+    }
+
+    private static void connectSecurity(IManagedGridNode from, IManagedGridNode to, Level level) {
+        to.addService(ISecurityProvider.class, new SecurityBridge(from, level));
     }
 
     private static class SecurityBridge implements ISecurityProvider {
 
         private final IManagedGridNode node;
+        private final Level level;
 
-        public SecurityBridge(IManagedGridNode from) {
+        public SecurityBridge(IManagedGridNode from, Level level) {
             node = from;
+            this.level = level;
         }
 
-        private SecurityService getMainSecurityService() {
-            return getSecurityService(node);
+        public SecurityStationBlockEntity getSecurityStation() {
+            var securityService = getSecurityService();
+            if (securityService == null)
+                return null;
+            return (SecurityStationBlockEntity) Locatables.securityStations().get(level,
+                    securityService.getSecurityKey());
+        }
+
+        private SecurityService getSecurityService() {
+            if (node.getGrid() == null)
+                return null;
+            if (!(node.getGrid().getSecurityService() instanceof SecurityService securityService))
+                return null;
+            return securityService;
         }
 
         @Override
         public long getSecurityKey() {
-            if (getMainSecurityService() == null)
+            var securityStation = getSecurityStation();
+            if (securityStation == null)
                 return 0;
-            return getMainSecurityService().getSecurityKey();
+            return securityStation.getSecurityKey();
         }
 
         @Override
@@ -58,16 +74,18 @@ public class SecurityFiberPart extends QuartzFiberPart {
 
         @Override
         public boolean isSecurityEnabled() {
-            if (getMainSecurityService() == null)
+            var securityStation = getSecurityStation();
+            if (securityStation == null)
                 return false;
-            return getMainSecurityService().isAvailable();
+            return securityStation.isSecurityEnabled();
         }
 
         @Override
         public int getOwner() {
-            if (getMainSecurityService() == null)
-                return 0;
-            return getMainSecurityService().getOwner();
+            var securityStation = getSecurityStation();
+            if (securityStation == null)
+                return -1;
+            return securityStation.getOwner();
         }
     }
 }
