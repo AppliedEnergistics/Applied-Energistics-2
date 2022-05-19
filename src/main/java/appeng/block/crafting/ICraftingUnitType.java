42012b46f42c9d2838af8ef705c2b02326bba29c
90 <90@national.shitposting.agency>
2022-05-19 12:42:31 +0100

    Add ICraftingUnitType javadoc

--- src/main/java/appeng/block/crafting/ICraftingUnitType.java
+++ src/main/java/appeng/block/crafting/ICraftingUnitType.java
@@ -1,12 +1,29 @@
 package appeng.block.crafting;
 
+/**
+ * Implemented by classes/enums meant to provide their own types of crafting CPU blocks.
+ */
 public interface ICraftingUnitType {
+
+    /**
+     * @return the capacity of a given crafting <i>storage</i> block in bytes (should be 0 if not storage).
+     */
     int getStorageBytes();
 
+    /**
+     * @return whether a given crafting unit type corresponds with a crafting <i>accelerator</i>.
+     */
     boolean isAccelerator();
 
+    /**
+     * @return whether a given crafting unit type corresponds with a crafting <i>monitor</i>.
+     */
     boolean isStatus();
 
+    /**
+     * @return whether a given crafting unit type corresponds with a crafting <i>storage</i> block.
+     * By default, anything with a (positive) non-zero storage capacity in bytes is assumed to be a storage block.
+     */
     default boolean isStorage() {
         return this.getStorageBytes() > 0;
     }
