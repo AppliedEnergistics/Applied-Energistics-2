27c4e976962f500dad0e7c1b984ceeccdbe2d488
Sebastian Hartte <shartte@users.noreply.github.com>
2022-07-30 23:16:13 +0200

    Fixes #6464: Use a different injection point to avoid repeated calls to the model bakeries top-level map causing AE2 to repeatedly apply its model customizations.

--- src/main/java/appeng/mixins/spatial/MinecraftServerMixin.java
+++ src/main/java/appeng/mixins/spatial/MinecraftServerMixin.java
@@ -5,13 +5,13 @@ import java.util.concurrent.Executor;
 
 import com.google.common.collect.ImmutableList;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
