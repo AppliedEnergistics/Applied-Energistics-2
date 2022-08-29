cba2463d6f5ebd4f2237228b257c1e2d2c6e86cb
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-08-29 16:31:44 +0200

    You spin me right round
    - Fix #5934: Rotating a storage monitor does not rotate the item inside.
    - Fix #6374: Rotate the displayed item when a crafting monitor is rotated.
    - Make rotation of AE2 blocks a bit more consistent (always clockwise).
    - Fix coloring issue with crafting monitor model.

--- src/main/java/appeng/util/Platform.java
+++ src/main/java/appeng/util/Platform.java
@@ -341,97 +341,11 @@ public class Platform {
     }
 
     public static Direction rotateAround(Direction forward, Direction axis) {
-        switch (forward) {
-            case DOWN:
-                switch (axis) {
-                    case DOWN:
-                        return forward;
-                    case UP:
-                        return forward;
-                    case NORTH:
-                        return Direction.EAST;
-                    case SOUTH:
-                        return Direction.WEST;
-                    case EAST:
-                        return Direction.NORTH;
-                    case WEST:
-                        return Direction.SOUTH;
-                    default:
-                        break;
-                }
-                break;
-            case UP:
-                switch (axis) {
-                    case NORTH:
-                        return Direction.WEST;
-                    case SOUTH:
-                        return Direction.EAST;
-                    case EAST:
-                        return Direction.SOUTH;
-                    case WEST:
-                        return Direction.NORTH;
-                    default:
-                        break;
-                }
-                break;
-            case NORTH:
-                switch (axis) {
-                    case UP:
-                        return Direction.WEST;
-                    case DOWN:
-                        return Direction.EAST;
-                    case EAST:
-                        return Direction.UP;
-                    case WEST:
-                        return Direction.DOWN;
-                    default:
-                        break;
-                }
-                break;
-            case SOUTH:
-                switch (axis) {
-                    case UP:
-                        return Direction.EAST;
-                    case DOWN:
-                        return Direction.WEST;
-                    case EAST:
-                        return Direction.DOWN;
-                    case WEST:
-                        return Direction.UP;
-                    default:
-                        break;
-                }
-                break;
-            case EAST:
-                switch (axis) {
-                    case UP:
-                        return Direction.NORTH;
-                    case DOWN:
-                        return Direction.SOUTH;
-                    case NORTH:
-                        return Direction.UP;
-                    case SOUTH:
-                        return Direction.DOWN;
-                    default:
-                        break;
-                }
-            case WEST:
-                switch (axis) {
-                    case UP:
-                        return Direction.SOUTH;
-                    case DOWN:
-                        return Direction.NORTH;
-                    case NORTH:
-                        return Direction.DOWN;
-                    case SOUTH:
-                        return Direction.UP;
-                    default:
-                        break;
-                }
-            default:
-                break;
+        if (forward.getAxis() == axis.getAxis()) {
+            return forward;
         }
-        return forward;
+        var newForward = forward.getNormal().cross(axis.getNormal());
+        return Objects.requireNonNull(Direction.fromNormal(new BlockPos(newForward)));
     }
 
     public static boolean securityCheck(GridNode a, GridNode b) {
