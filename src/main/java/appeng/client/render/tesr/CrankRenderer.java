9aa7756a24cd843132bda5b22d2cf25ef425d2d6
Sebastian Hartte <shartte@users.noreply.github.com>
2022-09-12 01:20:09 +0200

    Added a much nicer crank model (thanks Sea_Kerman!)

--- src/main/java/appeng/client/render/tesr/CrankRenderer.java
+++ src/main/java/appeng/client/render/tesr/CrankRenderer.java
@@ -28,17 +28,27 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 
 import appeng.client.render.FacingToRotation;
+import appeng.core.AELog;
+import appeng.core.AppEng;
 import appeng.tile.grindstone.CrankBlockEntity;
 
 @Environment(EnvType.CLIENT)
 public class CrankRenderer implements BlockEntityRenderer<CrankBlockEntity> {
 
+    public static final ResourceLocation BASE_MODEL = AppEng.makeId("block/crank_base");
+    public static final ResourceLocation HANDLE_MODEL = AppEng.makeId("block/crank_handle");
+
     private final BlockRenderDispatcher blockRenderer;
 
+    private final ModelManager modelManager;
+
     public CrankRenderer(BlockEntityRendererProvider.Context context) {
+        this.modelManager = context.getBlockRenderDispatcher().getBlockModelShaper().getModelManager();
         this.blockRenderer = context.getBlockRenderDispatcher();
     }
 
@@ -46,21 +56,48 @@ public class CrankRenderer implements BlockEntityRenderer<CrankBlockEntity> {
     public void render(CrankBlockEntity crank, float partialTick, PoseStack stack, MultiBufferSource buffers,
             int packedLight, int packedOverlay) {
 
+        var baseModel = modelManager.bakedRegistry.get(BASE_MODEL);
+        if (baseModel == null) {
+            AELog.warn("Crank base model missing");
+            return;
+        }
+        var handleModel = modelManager.bakedRegistry.get(HANDLE_MODEL);
+        if (handleModel == null) {
+            AELog.warn("Crank handle model missing");
+            return;
+        }
+
+        var blockState = crank.getBlockState();
+        var buffer = buffers.getBuffer(RenderType.cutout());
+        var pos = crank.getBlockPos();
+
         // Apply GL transformations relative to the center of the block: 1) TE rotation
         // and 2) crank rotation
         stack.pushPose();
         stack.translate(0.5, 0.5, 0.5);
         FacingToRotation.get(crank.getForward(), crank.getUp()).push(stack);
+        stack.translate(-0.5, -0.5, -0.5);
+
+        // Render the base model followed by the actual crank model
+        blockRenderer.getModelRenderer().tesselateWithAO(
+                crank.getLevel(),
+                baseModel,
+                blockState,
+                pos,
+                stack,
+                buffer,
+                false,
+                RandomSource.create(),
+                blockState.getSeed(pos),
+                packedOverlay);
+
+        stack.translate(0.5, 0.5, 0.5);
         stack.mulPose(new Quaternion(0, crank.getVisibleRotation(), 0, true));
         stack.translate(-0.5, -0.5, -0.5);
 
-        var blockState = crank.getBlockState();
-        var model = blockRenderer.getBlockModel(blockState);
-        var buffer = buffers.getBuffer(RenderType.cutout());
-        var pos = crank.getBlockPos();
         blockRenderer.getModelRenderer().tesselateWithAO(
                 crank.getLevel(),
-                model,
+                handleModel,
                 blockState,
                 pos,
                 stack,
@@ -71,4 +108,5 @@ public class CrankRenderer implements BlockEntityRenderer<CrankBlockEntity> {
                 packedOverlay);
         stack.popPose();
     }
+
 }
