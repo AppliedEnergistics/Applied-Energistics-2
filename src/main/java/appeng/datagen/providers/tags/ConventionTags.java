494a0aa96cba79b4ec0d5c5136875880d752fd9b
Sebastian Hartte <shartte@users.noreply.github.com>
2022-09-16 18:40:25 +0200

    Allow color to be removed from cables and memory cards with snowballs in addition to water buckets. Since snowballs can be stacked, this should make removal from many cables easier.

--- src/main/java/appeng/datagen/providers/tags/ConventionTags.java
+++ src/main/java/appeng/datagen/providers/tags/ConventionTags.java
@@ -87,7 +87,6 @@ public final class ConventionTags {
 
     public static TagKey<Item> ENDER_PEARL = tag("c:ender_pearls");
     public static TagKey<Item> ENDER_PEARL_DUST = tag("c:ender_pearl_dusts");
-    public static TagKey<Item> WHEAT_CROP = tag("c:wheat_crops");
 
     public static TagKey<Item> WOOD_STICK = tag("c:wooden_rods");
     public static TagKey<Item> CHEST = tag("c:wooden_chests");
@@ -114,6 +113,10 @@ public final class ConventionTags {
     public static TagKey<Item> PAINT_BALLS = tag("ae2:paint_balls");
     public static TagKey<Item> MEMORY_CARDS = tag("ae2:memory_cards");
     public static TagKey<Item> INSCRIBER_PRESSES = tag("ae2:inscriber_presses");
+    /**
+     * Items that can be used in recipes to remove color from colored items.
+     */
+    public static TagKey<Item> CAN_REMOVE_COLOR = tag("ae2:can_remove_color");
 
     // Budding stuff
     public static final TagKey<Item> BUDDING_BLOCKS = tag("c:budding_blocks");
