89dfac492ebab79358e34c6e7982768c1bb42849
Sebastian Hartte <shartte@users.noreply.github.com>
2022-09-25 23:04:05 +0200

    Added a visual preview of content and upgrades to tooltips of storage cells, matter cannon and color applicator.

--- src/main/java/appeng/me/cells/BasicCellHandler.java
+++ src/main/java/appeng/me/cells/BasicCellHandler.java
@@ -18,16 +18,24 @@
 
 package appeng.me.cells;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 
 import appeng.api.config.IncludeExclude;
+import appeng.api.stacks.GenericStack;
 import appeng.api.storage.cells.ICellHandler;
 import appeng.api.storage.cells.ISaveProvider;
+import appeng.core.AEConfig;
 import appeng.core.localization.GuiText;
 import appeng.core.localization.Tooltips;
+import appeng.items.storage.StorageCellTooltipComponent;
 
 /**
  * Cell handler that manages all normal storage cells (items, fluids).
@@ -68,4 +76,64 @@ public class BasicCellHandler implements ICellHandler {
         }
     }
 
+    public Optional<TooltipComponent> getTooltipImage(ItemStack is) {
+        var handler = getCellInventory(is, null);
+        if (handler == null) {
+            return Optional.empty();
+        }
+
+        var upgradeStacks = new ArrayList<ItemStack>();
+        if (AEConfig.instance().isTooltipShowCellUpgrades()) {
+            for (var upgrade : handler.getUpgradesInventory()) {
+                upgradeStacks.add(upgrade);
+            }
+        }
+
+        // Find items with the highest stored amount
+        boolean hasMoreContent;
+        List<GenericStack> content;
+        if (AEConfig.instance().isTooltipShowCellContent()) {
+            content = new ArrayList<>();
+
+            var maxCountShown = AEConfig.instance().getTooltipMaxCellContentShown();
+
+            var availableStacks = handler.getAvailableStacks();
+            for (var entry : availableStacks) {
+                content.add(new GenericStack(entry.getKey(), entry.getLongValue()));
+            }
+
+            // Fill up with stacks from the filter if it's not inverted
+            if (content.size() < maxCountShown && handler.getPartitionListMode() == IncludeExclude.WHITELIST) {
+                var config = handler.getConfigInventory();
+                for (int i = 0; i < config.size(); i++) {
+                    var what = config.getKey(i);
+                    if (what != null) {
+                        // Don't add it twice
+                        if (availableStacks.get(what) <= 0) {
+                            content.add(new GenericStack(what, 0));
+                        }
+                    }
+                    if (content.size() > maxCountShown) {
+                        break; // Don't need to add filters beyond 6 (to determine if it has more than 5 below)
+                    }
+                }
+            }
+
+            // Sort by amount descending
+            content.sort(Comparator.comparingLong(GenericStack::amount).reversed());
+
+            hasMoreContent = content.size() > maxCountShown;
+            if (content.size() > maxCountShown) {
+                content.subList(maxCountShown, content.size()).clear();
+            }
+        } else {
+            hasMoreContent = false;
+            content = Collections.emptyList();
+        }
+
+        return Optional.of(new StorageCellTooltipComponent(
+                upgradeStacks,
+                content,
+                hasMoreContent));
+    }
 }
