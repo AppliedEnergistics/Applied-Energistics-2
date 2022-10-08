516d1192347ce8ec93e1cedc99c3d15e7aa0ffe5
Sebastian Hartte <shartte@users.noreply.github.com>
2022-10-08 12:09:34 +0200

    Make the Create Mixin conditional on Create being loaded.

---
+++ src/main/java/appeng/mixins/ConfigPlugin.java
@@ -0,0 +1,52 @@
+package appeng.mixins;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+/**
+ * Disables the Create compatibility mixin if create isn't loaded.
+ */
+public class ConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @SuppressWarnings("ReturnOfNull")
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if ("appeng.mixins.PonderWorldMixin".equals(mixinClassName)) {
+            var instance = FabricLoader.getInstance();
+            return instance.isModLoaded("create");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @SuppressWarnings("ReturnOfNull")
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
