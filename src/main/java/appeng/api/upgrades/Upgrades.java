c28db9f16f6440082218790a40dcfec8ce3d0162
90 <90@national.shitposting.agency>
2022-10-13 20:02:58 +0100

    dammit

--- src/main/java/appeng/api/upgrades/Upgrades.java
+++ src/main/java/appeng/api/upgrades/Upgrades.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.Nonnegative;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
