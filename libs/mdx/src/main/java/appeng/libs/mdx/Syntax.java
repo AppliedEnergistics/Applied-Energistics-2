9c9f90e8a42f9f3cc4b1d5f848406ae507465c01
Sebastian Hartte <shartte@users.noreply.github.com>
2022-10-30 15:39:35 +0100

    MDX Extension

--- libs/mdx/src/main/java/appeng/libs/mdx/Syntax.java
+++ libs/mdx/src/main/java/appeng/libs/mdx/Syntax.java
@@ -3,6 +3,7 @@ package appeng.libs.mdx;
 import appeng.libs.micromark.Extension;
 import appeng.libs.micromark.symbol.Codes;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Syntax {
@@ -12,6 +13,12 @@ public class Syntax {
     static {
         EXTENSION.flow.put(Codes.lessThan, List.of(JsxFlow.INSTANCE));
         EXTENSION.text.put(Codes.lessThan, List.of(JsxText.INSTANCE));
+
+        // See https://github.com/micromark/micromark-extension-mdx-md/blob/main/index.js
+        Collections.addAll(
+                EXTENSION.nullDisable,
+                "autolink", "codeIndented", "htmlFlow", "htmlText"
+        );
     }
 
 }
