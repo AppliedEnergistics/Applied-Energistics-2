8bb159ecb9e7ee883729d29a6e929fa428ae5103
Sebastian Hartte <shartte@users.noreply.github.com>
2022-12-09 16:09:20 +0100

    Fixes for test execution.

--- src/test/java/net/fabricmc/loader/impl/launch/AccessWidenerTransformer.java
+++ src/test/java/net/fabricmc/loader/impl/launch/AccessWidenerTransformer.java
@@ -1,15 +1,22 @@
 package net.fabricmc.loader.impl.launch;
 
+import com.google.common.collect.Sets;
+import net.bytebuddy.dynamic.loading.ClassInjector;
 import net.fabricmc.accesswidener.AccessWidener;
 import net.fabricmc.accesswidener.AccessWidenerClassVisitor;
 import net.fabricmc.loader.impl.FabricLoaderImpl;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.transformer.IMixinTransformer;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Applies AccessWideners using the Java Instrumentation API.
@@ -19,10 +26,37 @@ class AccessWidenerTransformer implements ClassFileTransformer {
     private final AccessWidener accessWidener;
 
     private final IMixinTransformer mixinTransformer;
+    private final ClassInjector classInjector;
+    private final Map<String, ?> syntheticClasses;
+    private final Set<String> injectedClasses = new HashSet<>();
 
-    public AccessWidenerTransformer(AccessWidener accessWidener, IMixinTransformer mixinTransformer) {
+    public AccessWidenerTransformer(AccessWidener accessWidener, IMixinTransformer mixinTransformer, ClassInjector classInjector) {
         this.accessWidener = accessWidener;
         this.mixinTransformer = mixinTransformer;
+        this.classInjector = classInjector;
+        this.syntheticClasses = getSyntheticClassMap(mixinTransformer);
+    }
+
+    /**
+     * Gets the private map of synthetic classes from the Mixin transformer.
+     * Since we only get callbacks in this class to transform classes that *actually* exist,
+     * we need to be proactive in injecting those synthetic classes and need access
+     * to the list of such classes that Mixin generated.
+     */
+    @SuppressWarnings("unchecked")
+    private Map<String, ?> getSyntheticClassMap(IMixinTransformer mixinTransformer) {
+        final Map<String, ?> syntheticClasses;
+        try {
+            var syntheticClassRegistryField = mixinTransformer.getClass().getDeclaredField("syntheticClassRegistry");
+            syntheticClassRegistryField.setAccessible(true);
+            var syntheticClassRegistry = syntheticClassRegistryField.get(mixinTransformer);
+            var classesField = syntheticClassRegistry.getClass().getDeclaredField("classes");
+            classesField.setAccessible(true);
+            syntheticClasses = (Map<String, ?>) classesField.get(syntheticClassRegistry);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return syntheticClasses;
     }
 
     @Override
@@ -38,8 +72,27 @@ class AccessWidenerTransformer implements ClassFileTransformer {
         }
 
         if (className.startsWith("net/minecraft") || className.contains("mixin")) {
-            className = className.replace('/', '.');
-            return mixinTransformer.transformClassBytes(className, className, classfileBuffer);
+            var environment = MixinEnvironment.getCurrentEnvironment();
+
+            var transformResult = mixinTransformer.transformClass(environment, niceName, classfileBuffer);
+
+            // Handle any synthetic anonymous classes that Mixins may have generated
+            // We need to inject those into the class-loader using ByteBuddy since
+            // the instrumentation API does not support this natively.
+            Set<String> classesToInject = Sets.difference(syntheticClasses.keySet(), injectedClasses);
+            if (!classesToInject.isEmpty()) {
+                var newClasses = new HashMap<String, byte[]>();
+                for (var syntheticClass : new HashSet<>(classesToInject)) {
+                    injectedClasses.add(syntheticClass);
+                    newClasses.put(
+                            syntheticClass.replace('/', '.'),
+                            mixinTransformer.generateClass(environment, syntheticClass)
+                    );
+                }
+                classInjector.injectRaw(newClasses);
+            }
+
+            return transformResult;
         }
         return null;
     }
--- src/test/java/net/fabricmc/loader/impl/launch/LauncherAccessor.java
+++ src/test/java/net/fabricmc/loader/impl/launch/LauncherAccessor.java
@@ -2,13 +2,15 @@ package net.fabricmc.loader.impl.launch;
 
 import net.bytebuddy.agent.ByteBuddyAgent;
 
+import java.io.File;
+
 /**
  * Due to package-level visibility, we use this hack to access the loader internals.
  */
 public class LauncherAccessor {
     private static boolean initialized;
 
-    public synchronized static void init() throws Exception {
+    public synchronized static void init() {
         if (initialized) {
             return;
         }
