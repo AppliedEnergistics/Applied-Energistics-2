c3eeddbd04dbc9daa32dd7eccee28d98697c027b
90 <90@national.shitposting.agency>
2022-12-10 16:45:08 +0000

    Switch to Fabric entrypoint for datagen (#6812)
    * Switch to Fabric entrypoint for datagen

--- src/main/java/appeng/core/AppEngClientStartup.java
+++ src/main/java/appeng/core/AppEngClientStartup.java
@@ -2,14 +2,11 @@ package appeng.core;
 
 import net.fabricmc.api.ClientModInitializer;
 
-import appeng.datagen.DatagenEntrypoint;
-
 @SuppressWarnings("unused")
 public class AppEngClientStartup implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         new AppEngClient();
-        DatagenEntrypoint.runIfEnabled();
     }
 }
