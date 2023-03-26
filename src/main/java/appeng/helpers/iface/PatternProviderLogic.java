011d60d3a00c5be340ce1759e0a9973cdb291462
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-03-26 23:56:10 +0200

    Fix #6968: Make PPs round-robin across their sides (#6997)

--- src/main/java/appeng/helpers/iface/PatternProviderLogic.java
+++ src/main/java/appeng/helpers/iface/PatternProviderLogic.java
@@ -129,6 +129,7 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
     private UnlockCraftingEvent unlockEvent;
     @Nullable
     private GenericStack unlockStack;
+    private int roundRobinIndex = 0;
 
     @Nullable
     public PatternProviderLogic(IManagedGridNode mainNode, PatternProviderLogicHost host) {
@@ -276,6 +277,21 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
         return this.priority;
     }
 
+    /**
+     * Apply round-robin to list.
+     */
+    private <T> void rearrangeRoundRobin(List<T> list) {
+        if (list.isEmpty()) {
+            return;
+        }
+
+        roundRobinIndex %= list.size();
+        for (int i = 0; i < roundRobinIndex; ++i) {
+            list.add(list.get(i));
+        }
+        list.subList(0, roundRobinIndex).clear();
+    }
+
     @Override
     public boolean pushPattern(IPatternDetails patternDetails, KeyCounter[] inputHolder) {
         if (!sendList.isEmpty() || !this.mainNode.isActive() || !this.patterns.contains(patternDetails)) {
@@ -289,6 +305,11 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
             return false;
         }
 
+        record PushTarget(Direction direction, PatternProviderTarget target) {
+        }
+        var possibleTargets = new ArrayList<PushTarget>();
+
+        // Push to crafting machines first
         for (var direction : host.getTargets()) {
             var adjPos = be.getBlockPos().relative(direction);
             var adjBe = level.getBlockEntity(adjPos);
@@ -313,6 +334,17 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
             if (adapter == null)
                 continue;
 
+            possibleTargets.add(new PushTarget(direction, adapter));
+        }
+
+        // Rearrange for round-robin
+        rearrangeRoundRobin(possibleTargets);
+
+        // Push to other kinds of blocks
+        for (var target : possibleTargets) {
+            var direction = target.direction();
+            var adapter = target.target();
+
             if (this.isBlocking() && adapter.containsPatternInput(this.patternInputs)) {
                 continue;
             }
@@ -331,6 +363,7 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
                 onPushPatternSuccess(patternDetails);
                 this.sendDirection = direction;
                 this.sendStacksOut();
+                ++roundRobinIndex;
                 return true;
             }
         }
