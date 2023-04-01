dbe33567bb1083d827c0f125cf0f0071ccdbfbbd
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-04-01 17:28:50 +0200

    Make SavedData more resistant to crashes. Fixes #6976

---
+++ src/main/java/appeng/core/worlddata/AESavedData.java
@@ -0,0 +1,50 @@
+package appeng.core.worlddata;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.mojang.logging.LogUtils;
+
+import org.slf4j.Logger;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.level.saveddata.SavedData;
+
+/**
+ * Base class for all AE2 saved data to make them more resistant to crashes while writing. Thank you RS for the idea!
+ */
+public abstract class AESavedData extends SavedData {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @Override
+    public void save(File file) {
+        if (!this.isDirty()) {
+            return;
+        }
+
+        File tempFile = file.toPath().getParent().resolve(file.getName() + ".temp").toFile();
+
+        CompoundTag compoundTag = new CompoundTag();
+        compoundTag.put("data", this.save(new CompoundTag()));
+        NbtUtils.addCurrentDataVersion(compoundTag);
+        try {
+            // Write to temp file first.
+            NbtIo.writeCompressed(compoundTag, tempFile);
+            // Delete old file.
+            if (file.exists()) {
+                if (!file.delete()) {
+                    LOGGER.error("Could not delete old file {}", file);
+                }
+            }
+            // Rename temp file to the correct name.
+            if (!tempFile.renameTo(file)) {
+                LOGGER.error("Could not rename file {} to {}", tempFile, file);
+            }
+        } catch (IOException iOException) {
+            LOGGER.error("Could not save data {}", this, iOException);
+        }
+        this.setDirty(false);
+    }
+}
