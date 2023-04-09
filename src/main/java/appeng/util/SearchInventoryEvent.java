3e6771e76e9b3067c9de46264144606a080bf8b4
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-04-09 11:05:37 +0200

    update javadoc

--- src/main/java/appeng/util/SearchInventoryEvent.java
+++ src/main/java/appeng/util/SearchInventoryEvent.java
@@ -11,9 +11,9 @@ import net.minecraft.world.item.ItemStack;
 
 public class SearchInventoryEvent {
     /**
-     * Event fired when AE2 is looking for potential wireless terminal item stacks in a player inventory. By default,
-     * AE2 only looks at the 36 usual slots of the player inventory, use this event to make AE2 consider more stacks.
-     * The stacks don't need to contain a wireless terminal, AE2 will check after the event.
+     * Event fired when AE2 is looking for ItemStacks in a player inventory. By default, AE2 only looks at the 36 usual
+     * slots of the player inventory, use this event to make AE2 consider more stacks. AE2 will check after the event if
+     * they contain the item it is searching.
      */
     public static final Event<BiConsumer<List<ItemStack>, Player>> EVENT = EventFactory.createArrayBacked(
             BiConsumer.class,
