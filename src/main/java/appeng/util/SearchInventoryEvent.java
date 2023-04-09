f1920c9a748441c94c7bcd0f04a0719cc7c69822
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-04-09 13:19:31 +0200

    Add event to find terminal for the "Crafting job completed" toasts (#7031)
    Fixes #6898.

---
+++ src/main/java/appeng/util/SearchInventoryEvent.java
@@ -0,0 +1,35 @@
+package appeng.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+public class SearchInventoryEvent {
+    /**
+     * Event fired when AE2 is looking for ItemStacks in a player inventory. By default, AE2 only looks at the 36 usual
+     * slots of the player inventory, use this event to make AE2 consider more stacks. AE2 will check after the event if
+     * they contain the item it is searching.
+     */
+    public static final Event<BiConsumer<List<ItemStack>, Player>> EVENT = EventFactory.createArrayBacked(
+            BiConsumer.class,
+            (events) -> (items, player) -> {
+                for (var event : events) {
+                    event.accept(items, player);
+                }
+            });
+
+    static {
+        EVENT.register((stacks, player) -> stacks.addAll(player.getInventory().items));
+    }
+
+    public static List<ItemStack> getItems(Player player) {
+        List<ItemStack> items = new ArrayList<>();
+        EVENT.invoker().accept(items, player);
+        return items;
+    }
+}
