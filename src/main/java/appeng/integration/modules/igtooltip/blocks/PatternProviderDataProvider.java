fb27b7521b8ea0379fe67d6ff912f65c851e321a
Jason <11360596+jpenilla@users.noreply.github.com>
2023-04-14 11:21:00 -0700

    Don't try to access AEKeyRendering server-side in PatternProviderDataProvider (#7044)

--- src/main/java/appeng/integration/modules/igtooltip/blocks/PatternProviderDataProvider.java
+++ src/main/java/appeng/integration/modules/igtooltip/blocks/PatternProviderDataProvider.java
@@ -12,6 +12,7 @@ import appeng.api.integrations.igtooltip.TooltipContext;
 import appeng.api.integrations.igtooltip.providers.BodyProvider;
 import appeng.api.integrations.igtooltip.providers.ServerDataProvider;
 import appeng.api.stacks.AmountFormat;
+import appeng.api.stacks.GenericStack;
 import appeng.core.localization.InGameTooltip;
 import appeng.helpers.iface.PatternProviderLogicHost;
 
@@ -22,12 +23,29 @@ public final class PatternProviderDataProvider
         implements BodyProvider<PatternProviderLogicHost>, ServerDataProvider<PatternProviderLogicHost> {
 
     private static final String NBT_LOCK_REASON = "craftingLockReason";
+    private static final String NBT_LOCK_UNTIL_RESULT_STACK = "craftingLockUntilResultStack";
 
     @Override
     public void buildTooltip(PatternProviderLogicHost host, TooltipContext context, TooltipBuilder tooltip) {
         var lockReason = context.serverData().getString(NBT_LOCK_REASON);
         if (!lockReason.isEmpty()) {
             tooltip.addLine(Component.Serializer.fromJson(lockReason));
+        }
+        var stack = context.serverData().getCompound(NBT_LOCK_UNTIL_RESULT_STACK);
+        if (!stack.isEmpty()) {
+            var genericStack = GenericStack.readTag(stack);
+            Component stackName;
+            Component stackAmount;
+            if (genericStack == null) {
+                stackName = new TextComponent("ERROR");
+                stackAmount = new TextComponent("ERROR");
+            } else {
+                stackName = AEStackRendering.getDisplayName(genericStack.what());
+                stackAmount = new TextComponent(
+                        genericStack.what().formatAmount(genericStack.amount(), AmountFormat.FULL));
+            }
+            tooltip.addLine(
+                    InGameTooltip.CraftingLockedUntilResult.text(stackName, stackAmount).withStyle(ChatFormatting.RED));
         }
     }
 
@@ -48,17 +66,15 @@ public final class PatternProviderDataProvider
             }
             case LOCK_UNTIL_RESULT -> {
                 var stack = logic.getUnlockStack();
-                Component stackName;
-                Component stackAmount;
                 if (stack != null) {
-                    stackName = AEStackRendering.getDisplayName(stack.what());
-                    stackAmount = new TextComponent(stack.what().formatAmount(stack.amount(), AmountFormat.FULL));
+                    serverData.put(NBT_LOCK_UNTIL_RESULT_STACK, GenericStack.writeTag(stack));
                 } else {
-                    stackName = new TextComponent("ERROR");
-                    stackAmount = new TextComponent("ERROR");
-
+                    // Put a non-empty compound tag, so we get "ERROR" when handling this on the client
+                    final CompoundTag errorDummy = new CompoundTag();
+                    errorDummy.putString("error", "error");
+                    serverData.put(NBT_LOCK_UNTIL_RESULT_STACK, errorDummy);
                 }
-                reason = InGameTooltip.CraftingLockedUntilResult.text(stackName, stackAmount);
+                return;
             }
         }
 
