78ebbe74193640018bdefbb1b819294c214f2b7e
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-04-14 23:19:08 +0200

    Fix #7012: Spatial anchor keeps loading extra chunks (#7046)

--- src/main/java/appeng/blockentity/spatial/SpatialAnchorBlockEntity.java
+++ src/main/java/appeng/blockentity/spatial/SpatialAnchorBlockEntity.java
@@ -183,8 +183,9 @@ public class SpatialAnchorBlockEntity extends AENetworkBlockEntity
     public void onMainNodeStateChanged(IGridNodeListener.State reason) {
         if (reason != IGridNodeListener.State.GRID_BOOT) {
             this.markForUpdate();
-            this.wakeUp();
         }
+
+        this.wakeUp();
     }
 
     @Override
@@ -226,12 +227,16 @@ public class SpatialAnchorBlockEntity extends AENetworkBlockEntity
     @Override
     public TickRateModulation tickingRequest(IGridNode node, int ticksSinceLastCall) {
         // Initialize once the network is ready and there are no entries marked as loaded.
-        if (!this.initialized && this.getMainNode().isOnline()) {
-            this.forceAll();
-            this.initialized = true;
-        } else {
-            this.cleanUp();
+        if (!this.initialized) {
+            if (!this.getMainNode().hasGridBooted()) {
+                // Wait for the end of pathing to give time for the network to load the first time...
+                return TickRateModulation.SAME;
+            } else {
+                this.initialized = true;
+            }
         }
+
+        this.cleanUp();
 
         // Be a bit lenient to not unload all chunks immediately upon power loss
         if (this.powerlessTicks > 200) {
@@ -353,16 +358,6 @@ public class SpatialAnchorBlockEntity extends AENetworkBlockEntity
         this.markForUpdate();
 
         return removed;
-    }
-
-    private void forceAll() {
-        getMainNode().ifPresent(grid -> {
-            var statistics = grid.getService(StatisticsService.class);
-            for (ChunkPos chunkPos : statistics.getChunks().get(this.getServerLevel())
-                    .elementSet()) {
-                this.force(chunkPos);
-            }
-        });
     }
 
     void releaseAll() {
