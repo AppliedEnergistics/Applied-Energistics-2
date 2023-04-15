b115bc037c20ff294670cb3e441a928a99708229
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-04-15 14:16:53 +0200

    Fix interfaces and PPs not saving after inv changes (#7050)

--- src/main/java/appeng/helpers/iface/PatternProviderLogic.java
+++ src/main/java/appeng/helpers/iface/PatternProviderLogic.java
@@ -144,6 +144,7 @@ public class PatternProviderLogic implements InternalInventoryHost, ICraftingPro
 
         this.returnInv = new PatternProviderReturnInventory(() -> {
             this.mainNode.ifPresent((grid, node) -> grid.getTickManager().alertDevice(node));
+            this.host.saveChanges();
         });
     }
 
