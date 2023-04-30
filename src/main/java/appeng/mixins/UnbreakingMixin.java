1f243e7c5d8f4e024843e6ba3e25b1d71061fac7
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-04-30 15:49:37 -0700

    spotless

--- src/main/java/appeng/mixins/UnbreakingMixin.java
+++ src/main/java/appeng/mixins/UnbreakingMixin.java
@@ -1,16 +1,17 @@
 package appeng.mixins;
 
-import appeng.core.definitions.AEParts;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.DigDurabilityEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.DigDurabilityEnchantment;
+
+import appeng.core.definitions.AEParts;
+
 /**
- * Adds the ME Annihilation Plane to the items to check for when applying
- * unbreaking.
+ * Adds the ME Annihilation Plane to the items to check for when applying unbreaking.
  */
 @Mixin(DigDurabilityEnchantment.class)
 public class UnbreakingMixin {
