6059ea997f4f672df29fb96121da07cc4926d4ee
Sebastian Hartte <shartte@users.noreply.github.com>
2023-05-30 01:13:31 +0200

    Use JUnit 5.10.0-M1 to work around class-loading issues

--- src/test/java/appeng/client/guidebook/layout/LayoutsTest.java
+++ src/test/java/appeng/client/guidebook/layout/LayoutsTest.java
@@ -3,7 +3,6 @@ package appeng.client.guidebook.layout;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
