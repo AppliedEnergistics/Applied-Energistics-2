a9237d1c7989456df7d0f44af45be35141505f61
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-08-04 13:32:24 +0200

    Remove wildcard

--- src/main/java/appeng/client/gui/me/common/Repo.java
+++ src/main/java/appeng/client/gui/me/common/Repo.java
@@ -19,7 +19,14 @@
 package appeng.client.gui.me.common;
 
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Nullable;
 
