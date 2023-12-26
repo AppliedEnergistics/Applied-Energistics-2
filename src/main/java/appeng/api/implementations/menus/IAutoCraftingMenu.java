eccf97c5f4c116beb2f9d5a4489f6040e7203ee2
Sebastian Hartte <shartte@users.noreply.github.com>
2023-12-26 22:43:08 +0100

    Minor refactoring.

--- src/main/java/appeng/api/implementations/menus/IAutoCraftingMenu.java
+++ src/main/java/appeng/api/implementations/menus/IAutoCraftingMenu.java
@@ -1,10 +1,10 @@
 package appeng.api.implementations.menus;
 
-import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.inventory.TransientCraftingContainer;
 
 /**
- * Marker interface for a {@linkplain CraftingContainer#menu menu stored in a crafting container} used by autocrafting.
- * Can be used to detect if a given {@link CraftingContainer} is being used for AE2 autocrafting.
+ * Marker interface for a {@linkplain TransientCraftingContainer#menu menu stored in a crafting container} used by
+ * autocrafting. Can be used to detect if a given {@link TransientCraftingContainer} is being used for AE2 autocrafting.
  */
 public interface IAutoCraftingMenu {
 }
