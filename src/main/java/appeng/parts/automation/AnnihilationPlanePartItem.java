2c65750aa84298fb663b3157278ec87d539fd1bd
90 <hi@90.gripe>
2023-12-31 16:00:58 +0100

    Add Silk Touch enchanted annihilation plane to creative tab (#7529)
    Closes #7527.
    
    (cherry picked from commit 9e4699d215d1b69a6de09fe662e3afa5c9420268)

--- src/main/java/appeng/parts/automation/AnnihilationPlanePartItem.java
+++ src/main/java/appeng/parts/automation/AnnihilationPlanePartItem.java
@@ -1,18 +1,22 @@
 package appeng.parts.automation;
 
 import java.util.List;
+import java.util.Map;
 
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 
 import appeng.core.localization.GuiText;
 import appeng.core.localization.Tooltips;
 import appeng.items.parts.PartItem;
+import appeng.util.EnchantmentUtil;
 
 /**
  * Special part item for {@link AnnihilationPlanePart} to handle enchants and extended tooltips.
@@ -54,5 +58,14 @@ public class AnnihilationPlanePartItem extends PartItem<AnnihilationPlanePart> {
         } else {
             lines.add(Tooltips.of(GuiText.IncreasedEnergyUseFromEnchants));
         }
+    }
+
+    @Override
+    public void addToMainCreativeTab(CreativeModeTab.Output output) {
+        super.addToMainCreativeTab(output);
+
+        var silkTouch = new ItemStack(this);
+        EnchantmentUtil.setEnchantments(silkTouch.getOrCreateTag(), Map.of(Enchantments.SILK_TOUCH, 1));
+        output.accept(silkTouch);
     }
 }
