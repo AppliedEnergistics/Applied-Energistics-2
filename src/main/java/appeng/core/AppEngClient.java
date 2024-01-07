db8f15514067d98c6b904015cad7d3864a21ac60
Sebastian Hartte <shartte@users.noreply.github.com>
2024-01-07 16:19:00 +0100

    Fix Clouds being visible in the Spatial Storage Dimension

--- src/main/java/appeng/core/AppEngClient.java
+++ src/main/java/appeng/core/AppEngClient.java
@@ -44,6 +44,7 @@ import net.minecraftforge.client.event.ModelEvent.RegisterGeometryLoaders;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
 import net.minecraftforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -94,6 +95,8 @@ import appeng.init.client.InitScreens;
 import appeng.init.client.InitStackRenderHandlers;
 import appeng.items.storage.StorageCellTooltipComponent;
 import appeng.siteexport.SiteExporter;
+import appeng.spatial.SpatialStorageDimensionIds;
+import appeng.spatial.SpatialStorageSkyProperties;
 import appeng.util.InteractionUtil;
 import appeng.util.Platform;
 
@@ -129,6 +132,7 @@ public class AppEngClient extends AppEngBase {
         modEventBus.addListener(this::registerEntityRenderers);
         modEventBus.addListener(this::registerEntityLayerDefinitions);
         modEventBus.addListener(this::registerHotkeys);
+        modEventBus.addListener(this::registerDimensionSpecialEffects);
 
         BlockAttackHook.install();
         RenderBlockOutlineHook.install();
@@ -156,6 +160,12 @@ public class AppEngClient extends AppEngBase {
                 Hotkeys.checkHotkeys();
             }
         });
+    }
+
+    private void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
+        event.register(
+                SpatialStorageDimensionIds.DIMENSION_TYPE_ID.location(),
+                SpatialStorageSkyProperties.INSTANCE);
     }
 
     private void registerClientCommands() {
