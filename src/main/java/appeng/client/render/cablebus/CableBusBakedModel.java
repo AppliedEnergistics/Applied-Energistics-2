3c478ce2ff59828b6bdfedd15b5ec4a0c6d4caa1
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-02-21 00:31:34 +0100

    Fix cable bus breaking showing particles with missing texture (#7643)
    (cherry picked from commit f8b8794cf86dd1f0707dca1db8c7678912aca5a3)
    (cherry picked from commit 6b2f0476fd717c548f27762cefc9861949aaa812)

--- src/main/java/appeng/client/render/cablebus/CableBusBakedModel.java
+++ src/main/java/appeng/client/render/cablebus/CableBusBakedModel.java
@@ -369,7 +369,7 @@ public class CableBusBakedModel implements BakedModel, FabricBakedModel {
     }
 
     private boolean isMissingTexture(TextureAtlasSprite particleTexture) {
-        return particleTexture.atlasLocation().equals(MissingTextureAtlasSprite.getLocation());
+        return particleTexture.contents().name().equals(MissingTextureAtlasSprite.getLocation());
     }
 
     @Override
