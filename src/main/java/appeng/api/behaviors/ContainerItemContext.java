2fb1ca4bc6a49158453d14cb5c202b8dbcc8c700
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-02-25 22:22:29 +0100

    Fix handling of container items that can contain multiple key types (#7666)
    For example gauge droppers from Mekanism.
    
    Fixes https://github.com/AppliedEnergistics/Applied-Mekanistics/issues/25.
    
    (cherry picked from commit 608620a59ef1f2d5b75bd8eb36337f558ba4e266)

--- src/main/java/appeng/api/behaviors/ContainerItemContext.java
+++ src/main/java/appeng/api/behaviors/ContainerItemContext.java
@@ -1,5 +1,7 @@
 package appeng.api.behaviors;
 
+import java.util.Map;
+
 import com.google.common.base.Preconditions;
 
 import org.jetbrains.annotations.Nullable;
@@ -11,38 +13,79 @@ import appeng.api.stacks.AEKey;
 import appeng.api.stacks.AEKeyType;
 import appeng.api.stacks.GenericStack;
 
-public class ContainerItemContext {
-    // slight generic abuse
-    private final ContainerItemStrategy<AEKey, Object> strategy;
-    private final Object context;
-    // used for sanity checking
-    private final AEKeyType type;
+public final class ContainerItemContext {
+    private final Map<AEKeyType, Entry<?>> entries;
 
-    protected ContainerItemContext(ContainerItemStrategy<AEKey, Object> strategy, Object context, AEKeyType type) {
-        this.strategy = strategy;
-        this.context = context;
-        this.type = type;
+    ContainerItemContext(Map<AEKeyType, Entry<?>> entries) {
+        this.entries = entries;
     }
 
     public @Nullable GenericStack getExtractableContent() {
-        return strategy.getExtractableContent(context);
+        for (var entry : entries.entrySet()) {
+            var content = entry.getValue().getExtractableContent();
+            if (content != null) {
+                return content;
+            }
+        }
+        return null;
+    }
+
+    private Entry<?> getEntry(AEKey key) {
+        var keyType = key.getType();
+        Preconditions.checkArgument(entries.containsKey(keyType), "Internal logic error: mismatched key and type");
+        return entries.get(keyType);
     }
 
     public long insert(AEKey key, long amount, Actionable mode) {
-        Preconditions.checkArgument(type.contains(key), "Internal logic error: mismatched key and type");
-        return strategy.insert(context, key, amount, mode);
+        return getEntry(key).insert(key, amount, mode);
     }
 
     public long extract(AEKey key, long amount, Actionable mode) {
-        Preconditions.checkArgument(type.contains(key), "Internal logic error: mismatched key and type");
-        return strategy.extract(context, key, amount, mode);
+        return getEntry(key).extract(key, amount, mode);
     }
 
-    public void playFillSound(Player player, AEKey what) {
-        strategy.playFillSound(player, what);
+    public void playFillSound(Player player, AEKey key) {
+        getEntry(key).playFillSound(player, key);
     }
 
-    public void playEmptySound(Player player, AEKey what) {
-        strategy.playEmptySound(player, what);
+    public void playEmptySound(Player player, AEKey key) {
+        getEntry(key).playEmptySound(player, key);
+    }
+
+    static class Entry<C> {
+        // slight generic abuse
+        private final ContainerItemStrategy<AEKey, C> strategy;
+        private final C context;
+        // used for sanity checking
+        private final AEKeyType type;
+
+        Entry(ContainerItemStrategy<AEKey, C> strategy, C context, AEKeyType type) {
+            this.strategy = strategy;
+            this.context = context;
+            this.type = type;
+        }
+
+        public @Nullable GenericStack getExtractableContent() {
+            return strategy.getExtractableContent(context);
+        }
+
+        public long insert(AEKey key, long amount, Actionable mode) {
+            Preconditions.checkArgument(type.contains(key), "Internal logic error: mismatched key and type");
+            return strategy.insert(context, key, amount, mode);
+        }
+
+        public long extract(AEKey key, long amount, Actionable mode) {
+            Preconditions.checkArgument(type.contains(key), "Internal logic error: mismatched key and type");
+            return strategy.extract(context, key, amount, mode);
+        }
+
+        public void playFillSound(Player player, AEKey what) {
+            strategy.playFillSound(player, what);
+        }
+
+        public void playEmptySound(Player player, AEKey what) {
+            strategy.playEmptySound(player, what);
+        }
+
     }
 }
