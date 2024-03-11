a2e751e2c9b988379402a1efaa9ed2887336b62d
shartte <shartte@users.noreply.github.com>
2024-03-11 03:28:45 +0100

    Fix use of memory cards on molecular assemblers (#7743)
    (cherry picked from commit 2325dd881c20678569a314a4e5bdbcba4aa3b6b8)

--- src/main/java/appeng/block/crafting/MolecularAssemblerBlock.java
+++ src/main/java/appeng/block/crafting/MolecularAssemblerBlock.java
@@ -18,10 +18,13 @@
 
 package appeng.block.crafting;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -58,19 +61,21 @@ public class MolecularAssemblerBlock extends AEBaseEntityBlock<MolecularAssemble
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player p, InteractionHand hand,
-            BlockHitResult hit) {
-        final MolecularAssemblerBlockEntity tg = this.getBlockEntity(level, pos);
-        if (tg != null && !InteractionUtil.isInAlternateUseMode(p)) {
-            if (!level.isClientSide()) {
-                hit.getDirection();
-                MenuOpener.open(MolecularAssemblerMenu.TYPE, p,
-                        MenuLocators.forBlockEntity(tg));
+    public InteractionResult onActivated(Level level, BlockPos pos, Player player, InteractionHand hand,
+            @Nullable ItemStack heldItem, BlockHitResult hit) {
+        var be = this.getBlockEntity(level, pos);
+        if (be != null) {
+
+            if (!InteractionUtil.isInAlternateUseMode(player)) {
+                if (!level.isClientSide()) {
+                    MenuOpener.open(MolecularAssemblerMenu.TYPE, player,
+                            MenuLocators.forBlockEntity(be));
+                }
+                return InteractionResult.sidedSuccess(level.isClientSide());
             }
-            return InteractionResult.sidedSuccess(level.isClientSide());
         }
 
-        return super.use(state, level, pos, p, hand, hit);
+        return InteractionResult.PASS;
     }
 
 }
