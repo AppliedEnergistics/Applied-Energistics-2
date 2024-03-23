db1dc6209e8669344e7e7c45046fba0fb21ec58f
Sebastian Hartte <shartte@users.noreply.github.com>
2024-03-23 12:07:05 +0100

    Name plate

--- src/main/java/appeng/items/materials/NamePressItem.java
+++ src/main/java/appeng/items/materials/NamePressItem.java
@@ -20,6 +20,7 @@ package appeng.items.materials;
 
 import java.util.List;
 
+import appeng.api.ids.AEComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
@@ -41,9 +42,9 @@ public class NamePressItem extends AEBaseItem {
             TooltipFlag advancedTooltips) {
         super.appendHoverText(stack, level, lines, advancedTooltips);
 
-        final CompoundTag c = stack.getOrCreateTag();
-        if (c.contains(TAG_INSCRIBE_NAME)) {
-            lines.add(Component.literal(c.getString(TAG_INSCRIBE_NAME)));
+        var inscribedName = stack.get(AEComponents.NAME_PRESS_NAME);
+        if (inscribedName != null) {
+            lines.add(inscribedName);
         }
     }
 }
