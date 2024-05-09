coding-1ab/modpack-season-2

Show the source of external content in the guidebook (#7784)

mods/Applied-Energistics-2/src/main/java/appeng/client/guidebook/GuideSourceWatcher.java
@@ -28,10 +28,10 @@ import io.methvin.watcher.DirectoryChangeListener;
 import io.methvin.watcher.DirectoryWatcher;
 
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.ModList;
 
 import appeng.client.guidebook.compiler.PageCompiler;
 import appeng.client.guidebook.compiler.ParsedGuidePage;
-import appeng.core.AppEng;
 
 class GuideSourceWatcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(GuideSourceWatcher.class);
@@ -40,6 +40,10 @@ class GuideSourceWatcher {
      * The {@link ResourceLocation} namespace to use for files in the watched folder.
      */
     private final String namespace;
+    /**
+     * The ID of the resource pack to use as the source pack.
+     */
+    private final String sourcePackId;
 
     private final Path sourceFolder;
 
@@ -55,6 +59,10 @@ class GuideSourceWatcher {
 
     public GuideSourceWatcher(String namespace, Path sourceFolder) {
         this.namespace = namespace;
+        // The namespace does not necessarily *need* to be a mod id, but if it is, the source pack needs to
+        // follow the specific mod-id format. Otherwise we assume it's a resource pack where namespace == pack id,
+        // which is also not 100% correct.
+        this.sourcePackId = ModList.get().isLoaded(namespace) ? "mod:" + namespace : namespace;
         this.sourceFolder = sourceFolder;
         if (!Files.isDirectory(sourceFolder)) {
             throw new RuntimeException("Cannot find the specified folder for the AE2 guidebook sources: "
@@ -133,7 +141,7 @@ class GuideSourceWatcher {
                 .map(entry -> {
                     var path = entry.getValue();
                     try (var in = Files.newInputStream(path)) {
-                        return PageCompiler.parse(AppEng.MOD_ID, entry.getKey(), in);
+                        return PageCompiler.parse(sourcePackId, entry.getKey(), in);
 
                     } catch (Exception e) {
                         LOGGER.error("Failed to reload guidebook page {}", path, e);
@@ -217,7 +225,7 @@ class GuideSourceWatcher {
         deletedPages.remove(pageId);
 
         try (var in = Files.newInputStream(path)) {
-            var page = PageCompiler.parse(AppEng.MOD_ID, pageId, in);
+            var page = PageCompiler.parse(sourcePackId, pageId, in);
             changedPages.put(pageId, page);
         } catch (Exception e) {
             LOGGER.error("Failed to reload guidebook page {}", path, e);

mods/Applied-Energistics-2/src/main/java/appeng/client/guidebook/screen/GuideScreen.java
@@ -22,14 +22,18 @@ import net.minecraft.client.gui.screens.inventory.tooltip.TooltipRenderUtil;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforgespi.language.IModInfo;
 
 import appeng.client.Point;
 import appeng.client.gui.DashPattern;
 import appeng.client.gui.DashedRectangle;
 import appeng.client.guidebook.Guide;
 import appeng.client.guidebook.GuidePage;
+import appeng.client.guidebook.GuidebookText;
 import appeng.client.guidebook.PageAnchor;
 import appeng.client.guidebook.PageCollection;
 import appeng.client.guidebook.color.ColorValue;
@@ -49,12 +53,15 @@ import appeng.client.guidebook.document.block.LytParagraph;
 import appeng.client.guidebook.document.flow.LytFlowAnchor;
 import appeng.client.guidebook.document.flow.LytFlowContainer;
 import appeng.client.guidebook.document.flow.LytFlowContent;
+import appeng.client.guidebook.document.flow.LytFlowSpan;
 import appeng.client.guidebook.document.interaction.GuideTooltip;
 import appeng.client.guidebook.document.interaction.InteractiveElement;
 import appeng.client.guidebook.layout.LayoutContext;
 import appeng.client.guidebook.layout.MinecraftFontMetrics;
 import appeng.client.guidebook.render.GuidePageTexture;
 import appeng.client.guidebook.render.SimpleRenderContext;
+import appeng.client.guidebook.style.TextAlignment;
+import appeng.client.guidebook.style.TextStyle;
 import appeng.core.AEConfig;
 import appeng.core.AppEng;
 
@@ -251,6 +258,8 @@ public class GuideScreen extends Screen {
 
         renderTitle(documentRect, context);
 
+        renderExternalpageSource(documentRect, context);
+
         super.render(guiGraphics, mouseX, mouseY, partialTick);
 
         poseStack.popPose();
@@ -260,6 +269,61 @@ public class GuideScreen extends Screen {
             renderTooltip(guiGraphics, mouseX, mouseY);
         }
 
+    }
+
+    private void renderExternalpageSource(LytRect documentRect, SimpleRenderContext context) {
+        // Render the source of the content
+        var externalSource = getExternalSourceName();
+        if (externalSource != null) {
+            var paragraph = new LytParagraph();
+            paragraph.appendText(GuidebookText.ContentFrom.text().getString() + " ");
+            var sourceSpan = new LytFlowSpan();
+
+            sourceSpan.appendText(externalSource);
+            sourceSpan.setStyle(TextStyle.builder().italic(true).build());
+            paragraph.append(sourceSpan);
+            paragraph.setStyle(TextStyle.builder().alignment(TextAlignment.RIGHT).build());
+            var layoutContext = new LayoutContext(new MinecraftFontMetrics());
+            paragraph.layout(layoutContext, documentRect.x(), documentRect.bottom(), documentRect.width());
+            var buffers = context.beginBatch();
+            paragraph.renderBatch(context, buffers);
+            context.endBatch(buffers);
+        }
+    }
+
+    /**
+     * Gets a readable name for the source of the page (i.e. resource pack name, mod name) if the page has been
+     * contributed externally.
+     */
+    @Nullable
+    private String getExternalSourceName() {
+        var sourcePackId = currentPage.sourcePack();
+        // If the pages came directly from a mod resource pack, we have to use the mod-list to resolve its name
+        if (sourcePackId.startsWith("mod:")) {
+            var modId = sourcePackId.substring("mod:".length());
+
+            // Only show the source marker for pages that are not native to the guides mod
+            if (guide.getDefaultNamespace().equals(modId)) {
+                return null;
+            }
+
+            return ModList.get().getModContainerById(modId)
+                    .map(ModContainer::getModInfo)
+                    .map(IModInfo::getDisplayName)
+                    .orElse(null);
+        }
+
+        // Only show the source marker for pages that are not native to the guides mod
+        if (guide.getDefaultNamespace().equals(sourcePackId)) {
+            return null;
+        }
+
+        var pack = Minecraft.getInstance().getResourcePackRepository().getPack(sourcePackId);
+        if (pack != null) {
+            return pack.getDescription().getString();
+        }
+
+        return null;
     }
 
     @Override

AppliedEnergistics/Applied-Energistics-2

src/main/java/appeng/client/guidebook/GuideSourceWatcher.java
@@ -19,6 +19,7 @@ import java.util.concurrent.Executors;
 import com.google.common.base.Stopwatch;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import net.minecraftforge.fml.ModList;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +32,6 @@ import net.minecraft.resources.ResourceLocation;
 
 import appeng.client.guidebook.compiler.PageCompiler;
 import appeng.client.guidebook.compiler.ParsedGuidePage;
-import appeng.core.AppEng;
 
 class GuideSourceWatcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(GuideSourceWatcher.class);
@@ -40,6 +40,10 @@ class GuideSourceWatcher {
      * The {@link ResourceLocation} namespace to use for files in the watched folder.
      */
     private final String namespace;
+    /**
+     * The ID of the resource pack to use as the source pack.
+     */
+    private final String sourcePackId;
 
     private final Path sourceFolder;
 
@@ -55,6 +59,10 @@ class GuideSourceWatcher {
 
     public GuideSourceWatcher(String namespace, Path sourceFolder) {
         this.namespace = namespace;
+        // The namespace does not necessarily *need* to be a mod id, but if it is, the source pack needs to
+        // follow the specific mod-id format. Otherwise we assume it's a resource pack where namespace == pack id,
+        // which is also not 100% correct.
+        this.sourcePackId = ModList.get().isLoaded(namespace) ? "mod:" + namespace : namespace;
         this.sourceFolder = sourceFolder;
         if (!Files.isDirectory(sourceFolder)) {
             throw new RuntimeException("Cannot find the specified folder for the AE2 guidebook sources: "
@@ -133,7 +141,7 @@ class GuideSourceWatcher {
                 .map(entry -> {
                     var path = entry.getValue();
                     try (var in = Files.newInputStream(path)) {
-                        return PageCompiler.parse(AppEng.MOD_ID, entry.getKey(), in);
+                        return PageCompiler.parse(sourcePackId, entry.getKey(), in);
 
                     } catch (Exception e) {
                         LOGGER.error("Failed to reload guidebook page {}", path, e);
@@ -217,7 +225,7 @@ class GuideSourceWatcher {
         deletedPages.remove(pageId);
 
         try (var in = Files.newInputStream(path)) {
-            var page = PageCompiler.parse(AppEng.MOD_ID, pageId, in);
+            var page = PageCompiler.parse(sourcePackId, pageId, in);
             changedPages.put(pageId, page);
         } catch (Exception e) {
             LOGGER.error("Failed to reload guidebook page {}", path, e);

src/main/java/appeng/client/guidebook/screen/GuideScreen.java
@@ -8,6 +8,9 @@ import java.util.function.Function;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.Tesselator;
 
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.forgespi.language.IModInfo;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +25,12 @@ import net.minecraft.client.gui.screens.inventory.tooltip.TooltipRenderUtil;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-
 import appeng.client.Point;
 import appeng.client.gui.DashPattern;
 import appeng.client.gui.DashedRectangle;
 import appeng.client.guidebook.Guide;
 import appeng.client.guidebook.GuidePage;
+import appeng.client.guidebook.GuidebookText;
 import appeng.client.guidebook.PageAnchor;
 import appeng.client.guidebook.PageCollection;
 import appeng.client.guidebook.color.ColorValue;
@@ -47,12 +50,15 @@ import appeng.client.guidebook.document.block.LytParagraph;
 import appeng.client.guidebook.document.flow.LytFlowAnchor;
 import appeng.client.guidebook.document.flow.LytFlowContainer;
 import appeng.client.guidebook.document.flow.LytFlowContent;
+import appeng.client.guidebook.document.flow.LytFlowSpan;
 import appeng.client.guidebook.document.interaction.GuideTooltip;
 import appeng.client.guidebook.document.interaction.InteractiveElement;
 import appeng.client.guidebook.layout.LayoutContext;
 import appeng.client.guidebook.layout.MinecraftFontMetrics;
 import appeng.client.guidebook.render.GuidePageTexture;
 import appeng.client.guidebook.render.SimpleRenderContext;
+import appeng.client.guidebook.style.TextAlignment;
+import appeng.client.guidebook.style.TextStyle;
 import appeng.core.AEConfig;
 import appeng.core.AppEng;
 
@@ -246,6 +252,8 @@ public class GuideScreen extends Screen {
 
         renderTitle(documentRect, context);
 
+        renderExternalpageSource(documentRect, context);
+
         super.render(guiGraphics, mouseX, mouseY, partialTick);
 
         poseStack.popPose();
@@ -255,6 +263,66 @@ public class GuideScreen extends Screen {
             renderTooltip(guiGraphics, mouseX, mouseY);
         }
 
+    }
+
+    private void renderExternalpageSource(LytRect documentRect, SimpleRenderContext context) {
+        // Render the source of the content
+        var externalSource = getExternalSourceName();
+        if (externalSource != null) {
+            var paragraph = new LytParagraph();
+            paragraph.appendText(GuidebookText.ContentFrom.text().getString() + " ");
+            var sourceSpan = new LytFlowSpan();
+
+            sourceSpan.appendText(externalSource);
+            sourceSpan.setStyle(TextStyle.builder().italic(true).build());
+            paragraph.append(sourceSpan);
+            paragraph.setStyle(TextStyle.builder().alignment(TextAlignment.RIGHT).build());
+            var layoutContext = new LayoutContext(new MinecraftFontMetrics());
+            paragraph.layout(layoutContext, documentRect.x(), documentRect.bottom(), documentRect.width());
+            var buffers = context.beginBatch();
+            paragraph.renderBatch(context, buffers);
+            context.endBatch(buffers);
+        }
+    }
+
+    /**
+     * Gets a readable name for the source of the page (i.e. resource pack name, mod name) if the page has been
+     * contributed externally.
+     */
+    @Nullable
+    private String getExternalSourceName() {
+        var sourcePackId = currentPage.sourcePack();
+        // If the pages came directly from a mod resource pack, we have to use the mod-list to resolve its name
+        if (sourcePackId.startsWith("mod:")) {
+            var modId = sourcePackId.substring("mod:".length());
+
+            // Only show the source marker for pages that are not native to the guides mod
+            if (guide.getDefaultNamespace().equals(modId)) {
+                return null;
+            }
+
+            return ModList.get().getModContainerById(modId)
+                    .map(ModContainer::getModInfo)
+                    .map(IModInfo::getDisplayName)
+                    .orElse(null);
+        }
+
+        // Only show the source marker for pages that are not native to the guides mod
+        if (guide.getDefaultNamespace().equals(sourcePackId)) {
+            return null;
+        }
+
+        var pack = Minecraft.getInstance().getResourcePackRepository().getPack(sourcePackId);
+        if (pack != null) {
+            return pack.getDescription().getString();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        // Stub this out otherwise vanilla renders a background on top of our content
     }
 
     private void renderTitle(LytRect documentRect, SimpleRenderContext context) {