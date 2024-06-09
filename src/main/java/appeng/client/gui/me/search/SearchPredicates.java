c94754d4aaba016ab174b760c31452aa92df970e
JoelMorrisey <joelmorrissey.2408@gmail.com>
2024-06-09 16:44:52 +1000

    * Added back tooltips cache * Added logic to make searchTooltips option work again

--- src/main/java/appeng/client/gui/me/search/SearchPredicates.java
+++ src/main/java/appeng/client/gui/me/search/SearchPredicates.java
@@ -4,22 +4,23 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import appeng.core.AEConfig;
 import appeng.menu.me.common.GridInventoryEntry;
 
 final class SearchPredicates {
 
-    static Predicate<GridInventoryEntry> fromString(String searchString) {
+    static Predicate<GridInventoryEntry> fromString(String searchString, RepoSearch repoSearch) {
         List<Predicate<GridInventoryEntry>> gridPredictions;
 
         String[] orParts = searchString.split("\\|");
 
         if (orParts.length == 1) {
-            gridPredictions = getPredicates(orParts[0]);
+            gridPredictions = getPredicates(orParts[0], repoSearch);
         } else {
             List<Predicate<GridInventoryEntry>> orPartFilters = new LinkedList<>();
 
             for (String orPart : orParts) {
-                orPartFilters.add(AndSearchPredicate.of(getPredicates(orPart)));
+                orPartFilters.add(AndSearchPredicate.of(getPredicates(orPart, repoSearch)));
             }
 
             gridPredictions = new LinkedList<>();
@@ -29,20 +30,25 @@ final class SearchPredicates {
         return AndSearchPredicate.of(gridPredictions);
     }
 
-    private static List<Predicate<GridInventoryEntry>> getPredicates(String query) {
+    private static List<Predicate<GridInventoryEntry>> getPredicates(String query, RepoSearch repoSearch) {
         List<Predicate<GridInventoryEntry>> predicateFilters = new LinkedList<>();
 
         for (String part : query.toLowerCase().trim().split(" ")) {
             if (part.startsWith("@")) {
                 predicateFilters.add(new ModSearchPredicate(part.substring(1)));
             } else if (part.startsWith("#")) {
-                predicateFilters.add(new TooltipsSearchPredicate(part.substring(1)));
+                predicateFilters.add(new TooltipsSearchPredicate(part.substring(1), repoSearch));
             } else if (part.startsWith("$")) {
                 predicateFilters.add(new TagSearchPredicate(part.substring(1)));
             } else if (part.startsWith("*")) {
                 predicateFilters.add(new ItemIdSearchPredicate(part.substring(1)));
             } else {
-                predicateFilters.add(new NameSearchPredicate(part));
+                if (AEConfig.instance().isSearchTooltips()) {
+                    // The tooltip includes the display name too
+                    predicateFilters.add(new TooltipsSearchPredicate(part, repoSearch));
+                } else {
+                    predicateFilters.add(new NameSearchPredicate(part));
+                }
             }
         }
 
