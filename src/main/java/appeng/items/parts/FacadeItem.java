b896cb01e9604957847b73faa66a13bd2aa53da9
90 <hi@90.gripe>
2024-07-01 11:39:16 +0100

    Sanitise texture item IDs for facades (#7905)
    Fixes an instant crash bug reported by the Prominence 2 modpack devs if
    a facade has somehow been obtained with a malformed texture item ID.

--- src/main/java/appeng/items/parts/FacadeItem.java
+++ src/main/java/appeng/items/parts/FacadeItem.java
@@ -22,6 +22,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
@@ -201,13 +202,16 @@ public class FacadeItem extends AEBaseItem implements IFacadeItem, AEToolItem {
     public ItemStack getTextureItem(ItemStack is) {
         CompoundTag nbt = is.getTag();
 
-        if (nbt == null) {
+        if (nbt == null || !nbt.contains(NBT_ITEM_ID, Tag.TAG_STRING)) {
             return ItemStack.EMPTY;
         }
 
-        ResourceLocation itemId = new ResourceLocation(nbt.getString(NBT_ITEM_ID));
-        Item baseItem = BuiltInRegistries.ITEM.get(itemId);
+        var itemId = ResourceLocation.tryParse(nbt.getString(NBT_ITEM_ID));
+        if (itemId == null) {
+            return ItemStack.EMPTY;
+        }
 
+        Item baseItem = BuiltInRegistries.ITEM.get(itemId);
         return new ItemStack(baseItem, 1);
     }
 
