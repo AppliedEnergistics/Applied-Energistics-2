81edee6270b143eaf4d53f0a8a95e5851a4ea0e6
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-12-07 15:12:55 -0700

    Fix pulse unlock mode being broken by a constant redstone signal (#8172)
    With this change pattern providers will check if they already have a
    redstone signal when locking, and if so they will wait for the signal to
    depower and then power again. This fixes odd behavior with a constant
    redstone signal and block updates leading to unlocking without a
    'pulse'.
    
    ![2024-08-28_17 50
    27](https://github.com/user-attachments/assets/f9725586-6202-42b8-bd6c-71414059f551)
    
    Fixes #7179
    
    ---------
    
    Co-authored-by: Sebastian Hartte <shartte@users.noreply.github.com>
    (cherry picked from commit a1211bc3614f47d3887655063d63b879b3eac065)
    (cherry picked from commit 7de2565fea6f70d88dc018ad2bdaf0fc9c8bcc71)

--- src/main/java/appeng/helpers/patternprovider/UnlockCraftingEvent.java
+++ src/main/java/appeng/helpers/patternprovider/UnlockCraftingEvent.java
@@ -4,6 +4,8 @@ package appeng.helpers.patternprovider;
  * The types of event that the pattern provider is waiting for to unlock crafting again.
  */
 public enum UnlockCraftingEvent {
-    PULSE,
-    RESULT
+    REDSTONE_POWER, // Waiting for redstone to be on (pulse blocking mode)
+    REDSTONE_PULSE, // Waiting for redstone to turn off and back on (pulse blocking mode, already powered when craft
+                    // started)
+    RESULT // Waiting for result
 }
