AppliedEnergistics/Applied-Energistics-2

commit 70838d74f81917ece64335db2aca3cb3a604b780

1.20.5 (#7793)

src/main/java/appeng/parts/reporting/AbstractMonitorPart.java
@@ -23,11 +23,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
@@ -47,7 +49,7 @@ import appeng.api.stacks.AEKey;
 import appeng.api.stacks.AmountFormat;
 import appeng.client.render.BlockEntityRenderHelper;
 import appeng.core.localization.PlayerMessages;
-import appeng.util.Platform;
+import appeng.util.InteractionUtil;
 
 /**
  * A basic subclass for any item monitor like display with an item icon and an amount.
@@ -117,31 +119,31 @@ public abstract class AbstractMonitorPart extends AbstractDisplayPart
     }
 
     @Override
-    public void readFromNBT(CompoundTag data) {
-        super.readFromNBT(data);
+    public void readFromNBT(CompoundTag data, HolderLookup.Provider registries) {
+        super.readFromNBT(data, registries);
 
         this.isLocked = data.getBoolean("isLocked");
 
         if (data.contains("configuredItem", Tag.TAG_COMPOUND)) {
-            this.configuredItem = AEKey.fromTagGeneric(data.getCompound("configuredItem"));
+            this.configuredItem = AEKey.fromTagGeneric(registries, data.getCompound("configuredItem"));
         } else {
             this.configuredItem = null;
         }
     }
 
     @Override
-    public void writeToNBT(CompoundTag data) {
-        super.writeToNBT(data);
+    public void writeToNBT(CompoundTag data, HolderLookup.Provider registries) {
+        super.writeToNBT(data, registries);
 
         data.putBoolean("isLocked", this.isLocked);
 
         if (this.configuredItem != null) {
-            data.put("configuredItem", this.configuredItem.toTagGeneric());
+            data.put("configuredItem", this.configuredItem.toTagGeneric(registries));
         }
     }
 
     @Override
-    public void writeToStream(FriendlyByteBuf data) {
+    public void writeToStream(RegistryFriendlyByteBuf data) {
         super.writeToStream(data);
 
         data.writeBoolean(this.isLocked);
@@ -154,7 +156,7 @@ public abstract class AbstractMonitorPart extends AbstractDisplayPart
     }
 
     @Override
-    public boolean readFromStream(FriendlyByteBuf data) {
+    public boolean readFromStream(RegistryFriendlyByteBuf data) {
         boolean needRedraw = super.readFromStream(data);
 
         var isLocked = data.readBoolean();
@@ -191,63 +193,54 @@ public abstract class AbstractMonitorPart extends AbstractDisplayPart
     }
 
     @Override
-    public boolean onPartActivate(Player player, InteractionHand hand, Vec3 pos) {
-        if (isClientSide()) {
-            return true;
-        }
-
-        if (!this.getMainNode().isActive()) {
-            return false;
-        }
-
-        if (!Platform.hasPermissions(this.getHost().getLocation(), player)) {
-            return false;
-        }
-
-        if (!this.isLocked) {
-            var eq = player.getItemInHand(hand);
-            if (AEItemKey.matches(this.configuredItem, eq)) {
-                // Already matches: try to swap to key contained in the item.
-                var containedStack = ContainerItemStrategies.getContainedStack(eq);
-                if (containedStack != null) {
-                    this.configuredItem = containedStack.what();
-                }
-            } else {
-                this.configuredItem = AEItemKey.of(eq);
+    public boolean onUseWithoutItem(Player player, Vec3 pos) {
+        if (InteractionUtil.isInAlternateUseMode(player)) {
+            if (isClientSide()) {
+                return true;
             }
-            this.configureWatchers();
-            this.getHost().markForSave();
-            this.getHost().markForUpdate();
-        } else {
-            return super.onPartActivate(player, hand, pos);
-        }
 
-        return true;
-    }
+            if (!this.getMainNode().isActive()) {
+                return false;
+            }
 
-    @Override
-    public boolean onPartShiftActivate(Player player, InteractionHand hand, Vec3 pos) {
-        if (isClientSide()) {
-            return true;
-        }
-
-        if (!this.getMainNode().isActive()) {
-            return false;
-        }
-
-        if (!Platform.hasPermissions(this.getHost().getLocation(), player)) {
-            return false;
-        }
-
-        if (player.getItemInHand(hand).isEmpty()) {
             this.isLocked = !this.isLocked;
             player.displayClientMessage(
                     (this.isLocked ? PlayerMessages.isNowLocked : PlayerMessages.isNowUnlocked).text(), true);
             this.getHost().markForSave();
             this.getHost().markForUpdate();
+            return true;
         }
 
-        return true;
+        return super.onUseWithoutItem(player, pos);
+    }
+
+    @Override
+    public boolean onUseItemOn(ItemStack heldItem, Player player, InteractionHand hand, Vec3 pos) {
+        if (!this.isLocked) {
+            if (isClientSide()) {
+                return true;
+            }
+
+            if (!this.getMainNode().isActive()) {
+                return false;
+            }
+
+            if (AEItemKey.matches(this.configuredItem, heldItem)) {
+                // Already matches: try to swap to key contained in the item.
+                var containedStack = ContainerItemStrategies.getContainedStack(heldItem);
+                if (containedStack != null) {
+                    this.configuredItem = containedStack.what();
+                }
+            } else {
+                this.configuredItem = AEItemKey.of(heldItem);
+            }
+            this.configureWatchers();
+            this.getHost().markForSave();
+            this.getHost().markForUpdate();
+            return true;
+        }
+
+        return super.onUseWithoutItem(player, pos);
     }
 
     // update the system...

src/main/java/appeng/parts/reporting/ConversionMonitorPart.java
@@ -69,7 +69,7 @@ public class ConversionMonitorPart extends AbstractMonitorPart implements ISubMe
     }
 
     @Override
-    public boolean onPartActivate(Player player, InteractionHand hand, Vec3 pos) {
+    public boolean onUseItemOn(ItemStack heldItem, Player player, InteractionHand hand, Vec3 pos) {
         if (isClientSide()) {
             return true;
         }
@@ -78,29 +78,37 @@ public class ConversionMonitorPart extends AbstractMonitorPart implements ISubMe
             return false;
         }
 
-        if (!Platform.hasPermissions(this.getHost().getLocation(), player)) {
-            return false;
-        }
-
-        final ItemStack eq = player.getItemInHand(hand);
-
         if (this.isLocked()) {
-            if (eq.isEmpty()) {
-                this.insertItem(player, hand, true);
-            } else if (InteractionUtil.canWrenchRotate(eq)
-                    && (this.getDisplayed() == null || !AEItemKey.matches(getDisplayed(), eq))) {
+            if (InteractionUtil.canWrenchRotate(heldItem)
+                    && (this.getDisplayed() == null || !AEItemKey.matches(getDisplayed(), heldItem))) {
                 // wrench it
-                return super.onPartActivate(player, hand, pos);
+                return super.onUseWithoutItem(player, pos);
             } else {
-                this.insertItem(player, hand, false);
+                this.insertItem(player, heldItem);
             }
-        } else if (this.getDisplayed() != null && AEItemKey.matches(getDisplayed(), eq)) {
-            this.insertItem(player, hand, false);
-        } else {
-            return super.onPartActivate(player, hand, pos);
+            return true;
+        } else if (this.getDisplayed() != null && AEItemKey.matches(getDisplayed(), heldItem)) {
+            this.insertItem(player, heldItem);
+            return true;
         }
 
-        return true;
+        return super.onUseItemOn(heldItem, player, hand, pos);
+    }
+
+    @Override
+    public boolean onUseWithoutItem(Player player, Vec3 pos) {
+        if (this.isLocked()) {
+            if (isClientSide()) {
+                return true;
+            }
+            if (!this.getMainNode().isActive()) {
+                return false;
+            }
+            this.insertAllItem(player);
+            return true;
+        }
+
+        return super.onUseWithoutItem(player, pos);
     }
 
     @Override
@@ -145,35 +153,37 @@ public class ConversionMonitorPart extends AbstractMonitorPart implements ISubMe
         return true;
     }
 
-    private void insertItem(Player player, InteractionHand hand, boolean allItems) {
+    private void insertAllItem(Player player) {
         getMainNode().ifPresent(grid -> {
             var energy = grid.getEnergyService();
             var cell = grid.getStorageService().getInventory();
 
-            if (allItems) {
-                if (getDisplayed() instanceof AEItemKey itemKey) {
-                    var inv = new PlayerInternalInventory(player.getInventory());
+            if (getDisplayed() instanceof AEItemKey itemKey) {
+                var inv = new PlayerInternalInventory(player.getInventory());
 
-                    for (int x = 0; x < inv.size(); x++) {
-                        var targetStack = inv.getStackInSlot(x);
-                        if (itemKey.matches(targetStack)) {
-                            var canExtract = inv.extractItem(x, targetStack.getCount(), true);
-                            if (!canExtract.isEmpty()) {
-                                var inserted = StorageHelper.poweredInsert(energy, cell, itemKey, canExtract.getCount(),
-                                        new PlayerSource(player, this));
-                                inv.extractItem(x, (int) inserted, false);
-                            }
+                for (int x = 0; x < inv.size(); x++) {
+                    var targetStack = inv.getStackInSlot(x);
+                    if (itemKey.matches(targetStack)) {
+                        var canExtract = inv.extractItem(x, targetStack.getCount(), true);
+                        if (!canExtract.isEmpty()) {
+                            var inserted = StorageHelper.poweredInsert(energy, cell, itemKey, canExtract.getCount(),
+                                    new PlayerSource(player, this));
+                            inv.extractItem(x, (int) inserted, false);
                         }
                     }
                 }
-            } else {
-                var input = player.getItemInHand(hand);
-                if (!input.isEmpty()) {
-                    var inserted = StorageHelper.poweredInsert(energy, cell, AEItemKey.of(input), input.getCount(),
-                            new PlayerSource(player, this));
-                    input.shrink((int) inserted);
-                }
             }
+        });
+    }
+
+    private void insertItem(Player player, ItemStack heldItem) {
+        getMainNode().ifPresent(grid -> {
+            var energy = grid.getEnergyService();
+            var cell = grid.getStorageService().getInventory();
+
+            var inserted = StorageHelper.poweredInsert(energy, cell, AEItemKey.of(heldItem), heldItem.getCount(),
+                    new PlayerSource(player, this));
+            heldItem.shrink((int) inserted);
         });
     }
 

commit 3555f559a59d226e976fc34abb74a785cb3b4103

Fix locking storage and conversion monitors (#8272)
Fix #8179

The bug was probably introduced during the rewrite in 70838d74

I'm still a bit confused how `onUseItemOn` and `onUseWithoutItem` should
interact. My findings were that `onUseItemOn` is always called first
(even with an empty hand) and only if the event was not consumed (by
returning true) `onUseWithoutItem` would be called. My intuition looking
at those function names would have been that `onUseItemOn` would never
be called with an empty hand in the first place.

The problem was, that the event that would potentially lead to the code
for locking and unlocking in the `onUseWithoutItem` function was
consumed earlier by the `onUseItemOn` function. I chose to check for the
`!InteractionUtil.isInAlternateUseMode(player)` condition to bypass
those event consumptions.

Another functionality that was broken due to a similar problem, that
could only be observed after fixing the locking, was the insert all
items behavior when right-clicking empty-handedly. This was fixed by the
additional check for `else if (!heldItem.isEmpty())` and moving the
return into that block so that the empty-handed case would not return
true and continue to run into the `onUseWithoutItem` code.

Another point for the review: I changed the `return
super.onUseWithoutItem(player, pos);` in `onUseWithoutItem` in
`AbstractMonitorPart` to `return super.onUseItemOn(heldItem, player,
hand, pos);` because it looked like a copy&paste error during the
rewrite. If that served a specific purpose feel free to restore it, the
fix should work without it.

src/main/java/appeng/parts/reporting/AbstractMonitorPart.java
@@ -216,7 +216,7 @@ public abstract class AbstractMonitorPart extends AbstractDisplayPart
 
     @Override
     public boolean onUseItemOn(ItemStack heldItem, Player player, InteractionHand hand, Vec3 pos) {
-        if (!this.isLocked) {
+        if (!this.isLocked && !InteractionUtil.isInAlternateUseMode(player)) {
             if (isClientSide()) {
                 return true;
             }
@@ -240,7 +240,7 @@ public abstract class AbstractMonitorPart extends AbstractDisplayPart
             return true;
         }
 
-        return super.onUseWithoutItem(player, pos);
+        return super.onUseItemOn(heldItem, player, hand, pos);
     }
 
     // update the system...

src/main/java/appeng/parts/reporting/ConversionMonitorPart.java
@@ -78,15 +78,15 @@ public class ConversionMonitorPart extends AbstractMonitorPart implements ISubMe
             return false;
         }
 
-        if (this.isLocked()) {
+        if (this.isLocked() && !InteractionUtil.isInAlternateUseMode(player)) {
             if (InteractionUtil.canWrenchRotate(heldItem)
                     && (this.getDisplayed() == null || !AEItemKey.matches(getDisplayed(), heldItem))) {
                 // wrench it
                 return super.onUseWithoutItem(player, pos);
-            } else {
+            } else if (!heldItem.isEmpty()) {
                 this.insertItem(player, heldItem);
+                return true;
             }
-            return true;
         } else if (this.getDisplayed() != null && AEItemKey.matches(getDisplayed(), heldItem)) {
             this.insertItem(player, heldItem);
             return true;
@@ -97,7 +97,7 @@ public class ConversionMonitorPart extends AbstractMonitorPart implements ISubMe
 
     @Override
     public boolean onUseWithoutItem(Player player, Vec3 pos) {
-        if (this.isLocked()) {
+        if (this.isLocked() && !InteractionUtil.isInAlternateUseMode(player)) {
             if (isClientSide()) {
                 return true;
             }