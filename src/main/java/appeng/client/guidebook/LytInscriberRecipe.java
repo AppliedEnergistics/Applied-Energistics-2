7dcff06ff3ba06fd02fb41b6ec01405ce8aadefa
Sebastian Hartte <shartte@users.noreply.github.com>
2025-02-22 22:47:36 +0100

    Update to GuideME changes.

--- src/main/java/appeng/client/guidebook/LytInscriberRecipe.java
+++ src/main/java/appeng/client/guidebook/LytInscriberRecipe.java
@@ -1,35 +1,26 @@
 
 package appeng.client.guidebook;
 
-import net.minecraft.world.item.crafting.RecipeHolder;
-
 import guideme.document.LytRect;
+import guideme.document.block.LytBox;
 import guideme.document.block.LytSlot;
-import guideme.document.block.recipes.LytRecipeBox;
 import guideme.layout.LayoutContext;
 import guideme.render.RenderContext;
 
 import appeng.recipes.handlers.InscriberRecipe;
 
-public class LytInscriberRecipe extends LytRecipeBox {
-    private final InscriberRecipe recipe;
-
+public class LytInscriberRecipe extends LytBox {
     private final LytSlot topSlot;
     private final LytSlot middleSlot;
     private final LytSlot bottomSlot;
     private final LytSlot resultSlot;
 
-    public LytInscriberRecipe(RecipeHolder<InscriberRecipe> holder) {
-        super(holder);
-        this.recipe = holder.value();
-
+    public LytInscriberRecipe(InscriberRecipe recipe) {
         append(topSlot = new LytSlot(recipe.getTopOptional()));
         append(middleSlot = new LytSlot(recipe.getMiddleInput()));
         append(bottomSlot = new LytSlot(recipe.getBottomOptional()));
         append(resultSlot = new LytSlot(recipe.getResultItem()));
         resultSlot.setLargeSlot(true);
-
-        setPadding(5);
     }
 
     @Override
@@ -45,10 +36,9 @@ public class LytInscriberRecipe extends LytRecipeBox {
     @Override
     public void render(RenderContext context) {
         var bounds = getBounds();
-        context.renderPanel(bounds);
 
         context.fillIcon(new LytRect(
-                bounds.x() + 23, bounds.y() + 12, 46, 50), AE2GuideAssets.INSCRIBER_ARROWS);
+                bounds.x() + 18, bounds.y() + 7, 46, 50), AE2GuideAssets.INSCRIBER_ARROWS);
 
         super.render(context);
     }
