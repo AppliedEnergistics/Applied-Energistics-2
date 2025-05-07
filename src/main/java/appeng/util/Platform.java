e0b438b780965625172e9388459e7ded43680f3a
Troy Cook <cookta2012@gmail.com>
2025-05-07 11:08:01 -0500

    Update Platform.java

--- src/main/java/appeng/util/Platform.java
+++ src/main/java/appeng/util/Platform.java
@@ -479,13 +479,13 @@ public class Platform {
 		 BlockState placedAgainst = Objects.requireNonNull(blockSnapshot.getLevel()).getBlockState(blockSnapshot.getPos());
 		 BlockEvent.EntityPlaceEvent event = new BlockEvent.EntityPlaceEvent(blockSnapshot, placedAgainst, player);
 		 boolean placeAllowed = !MinecraftForge.EVENT_BUS.post(event);
-		 return level.mayInteract(player,pos) && placeAllowed;
+		 return placeAllowed;
 	 }
 	 
 	 public static boolean canBreak(Level level, Player player, BlockPos pos) { 
 		    BlockState state = level.getBlockState(pos);
 		    BlockEvent.BreakEvent event = new BlockEvent.BreakEvent(level, pos, state, player);
 		    boolean breakAllowed = !MinecraftForge.EVENT_BUS.post(event);
-		    return level.mayInteract(player,pos) && breakAllowed;
+		    return breakAllowed;
 		}
 }
