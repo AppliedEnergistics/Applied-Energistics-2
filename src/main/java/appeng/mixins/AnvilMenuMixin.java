2f1d407b1d82d750197587e653b983336a4bdd85
Sebastian Hartte <shartte@users.noreply.github.com>
2025-09-09 00:46:17 +0200

    Fix combining of enchanted annihilation planes in the anvil

--- src/main/java/appeng/mixins/AnvilMenuMixin.java
+++ src/main/java/appeng/mixins/AnvilMenuMixin.java
@@ -1,26 +1,33 @@
 package appeng.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.inventory.ItemCombinerMenu;
 
-import appeng.parts.automation.AnnihilationPlanePartItem;
+import appeng.core.definitions.AEParts;
 
 /**
- * @see AnnihilationPlanePartItem#CALLING_DAMAGEABLE_FROM_ANVIL
+ * Workaround to make enchanted annihilation planes combinable in anvils to upgrade their enchantment levels.
  */
 @Mixin(AnvilMenu.class)
-public class AnvilMenuMixin {
-    @Inject(method = "createResult", at = @At(value = "INVOKE", target = "net/minecraft/world/item/ItemStack.isDamageableItem()Z", ordinal = 1))
-    public void setAnnihilationPlaneThreadLocal(CallbackInfo ci) {
-        AnnihilationPlanePartItem.CALLING_DAMAGEABLE_FROM_ANVIL.set(Boolean.TRUE);
+public abstract class AnvilMenuMixin extends ItemCombinerMenu {
+    public AnvilMenuMixin(int containerId, Inventory playerInventory) {
+        super(null, 0, null, null);
+        throw new AssertionError();
     }
 
-    @Inject(method = "createResult", at = @At(value = "INVOKE", target = "net/minecraft/world/item/ItemStack.isDamageableItem()Z", ordinal = 1, shift = At.Shift.AFTER))
-    public void clearAnnihilationPlaneThreadLocal(CallbackInfo ci) {
-        AnnihilationPlanePartItem.CALLING_DAMAGEABLE_FROM_ANVIL.set(null);
+    @ModifyExpressionValue(method = "createResult", at = @At(value = "INVOKE", target = "net/minecraft/world/item/ItemStack.isDamageableItem()Z", ordinal = 1))
+    public boolean setAnnihilationPlaneThreadLocal(boolean isDamageable) {
+        if (AEParts.ANNIHILATION_PLANE.is(inputSlots.getItem(0))
+                && AEParts.ANNIHILATION_PLANE.is(inputSlots.getItem(1))) {
+            return true; // Act damageable to allow combining items
+        } else {
+            return isDamageable;
+        }
     }
 }
