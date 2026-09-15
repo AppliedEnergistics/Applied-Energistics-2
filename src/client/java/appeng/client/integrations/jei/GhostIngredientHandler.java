d46b1c102b7d0122637851895f8e68619e2e8700
Mithi83 <28407460+Mithi83@users.noreply.github.com>
2026-09-15 17:57:38 +0200

    Fix JEI drag&drop highlights (#8971)

--- src/client/java/appeng/client/integrations/jei/GhostIngredientHandler.java
+++ src/client/java/appeng/client/integrations/jei/GhostIngredientHandler.java
@@ -122,7 +122,7 @@ class GhostIngredientHandler implements IGhostIngredientHandler<AEBaseScreen> {
         public ItemSlotTarget(IIngredientType<I> type, AEBaseScreen<?> screen, FakeSlot slot) {
             this.type = type;
             this.slot = slot;
-            this.area = new Rect2i(screen.getGuiLeft() + slot.x, screen.getGuiTop() + slot.y, 16, 16);
+            this.area = new Rect2i(screen.getGuiLeft() + slot.x, Math.max(0, screen.getGuiTop()) + slot.y, 16, 16);
         }
 
         @Override
